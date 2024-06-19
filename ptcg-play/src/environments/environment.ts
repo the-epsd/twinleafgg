b49c06ed26e3ffce0640dc795310637872fee000
Wheater <ericthurt@gmail.com>
2024-06-19 14:36:01 -0500

    fix environment

--- ptcg-play/src/environments/environment.ts
+++ ptcg-play/src/environments/environment.ts
@@ -1,20 +1,12 @@
 export const environment = {
-  /**
-   * Uncomment this for local dev
-   */
-  apiUrl: 'http://0.0.0.0:8080',
-
-  /**
-   * Uncomment this for prod
-   */
-  // apiUrl: 'https://play-server.twinleaf.gg',
-  timeout: 10000 * 1000,
-  production: false,
+  apiUrl: 'https://play-server.twinleaf.gg',
+  timeout: 30 * 1000,
+  production: true,
   apiVersion: 2,
   defaultPageSize: 50,
   allowServerChange: true,
   refreshTokenInterval: 60 * 60 * 1000,
-  enableImageCache: false,
+  enableImageCache: true,
   defaultLanguage: 'en',
   languages: { en: 'English', jp: 'Japanese', fr: 'French' },
 };
