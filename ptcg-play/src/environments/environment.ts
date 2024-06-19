9073685b67e344e7a837c31ec33f320481a6120a
Charlie Lockyer <charlielockyer@macbook-pro.mynetworksettings.com>
2024-06-18 20:39:30 -0400

    small fix to env

--- ptcg-play/src/environments/environment.ts
+++ ptcg-play/src/environments/environment.ts
@@ -1,5 +1,13 @@
 export const environment = {
-  apiUrl: 'https://play-server.twinleaf.gg',
+  /**
+   * Uncomment this for local dev
+   */
+  apiUrl: 'http://0.0.0.0:8080',
+
+  /**
+   * Uncomment this for prod
+   */
+  // apiUrl: 'https://play-server.twinleaf.gg',
   timeout: 10000 * 1000,
   production: false,
   apiVersion: 2,
