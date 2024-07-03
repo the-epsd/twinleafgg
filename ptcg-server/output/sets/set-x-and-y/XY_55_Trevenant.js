20bfd2ee1cbb9c72279a8ab934f0bbd61e917f64
TommyN17 <tommyn0017@gmail.com>
2024-07-02 23:39:35 -0400

    Trevenant XY

--- ptcg-server/output/sets/set-x-and-y/XY_55_Trevenant.js
+++ ptcg-server/output/sets/set-x-and-y/XY_55_Trevenant.js
@@ -5,8 +5,9 @@ const pokemon_card_1 = require("../../game/store/card/pokemon-card");
 const card_types_1 = require("../../game/store/card/card-types");
 const pokemon_types_1 = require("../../game/store/card/pokemon-types");
 const game_1 = require("../../game");
-const game_effects_1 = require("../../game/store/effects/game-effects");
 const play_card_effects_1 = require("../../game/store/effects/play-card-effects");
+const game_effects_1 = require("../../game/store/effects/game-effects");
+const attack_effects_1 = require("../../game/store/effects/attack-effects");
 class Trevenant extends pokemon_card_1.PokemonCard {
     constructor() {
         super(...arguments);
@@ -33,21 +34,15 @@ class Trevenant extends pokemon_card_1.PokemonCard {
         this.cardImage = 'assets/cardback.png';
         this.name = 'Trevenant';
         this.fullName = 'Trevenant XY';
-        this.OPPONENT_CANNOT_PLAY_ITEM_CARDS_MARKER = 'OPPONENT_CANNOT_PLAY_ITEM_CARDS_MARKER';
     }
     reduceEffect(store, state, effect) {
-        if (effect instanceof game_effects_1.PowerEffect && effect.power.powerType === pokemon_types_1.PowerType.ABILITY && effect.power.name === 'Forest\'s Curse') {
+        if (effect instanceof play_card_effects_1.PlayItemEffect) {
             const player = effect.player;
             const opponent = game_1.StateUtils.getOpponent(state, player);
-            console.log(player.active.getPokemonCard() !== this
-                && opponent.active.getPokemonCard() !== this);
-            //Trevenant isn't active poke
-            if (player.active.getPokemonCard() !== this
-                && opponent.active.getPokemonCard() !== this) {
-                opponent.marker.removeMarker(this.OPPONENT_CANNOT_PLAY_ITEM_CARDS_MARKER, this);
+            if (player.active.getPokemonCard() !== this && opponent.active.getPokemonCard() !== this) {
                 return state;
             }
-            // Try to reduce PowerEffect, to check if something is blocking our ability
+            // Checking to see if ability is being blocked
             try {
                 const stub = new game_effects_1.PowerEffect(player, {
                     name: 'test',
@@ -59,13 +54,27 @@ class Trevenant extends pokemon_card_1.PokemonCard {
             catch (_a) {
                 return state;
             }
-            opponent.marker.addMarker(this.OPPONENT_CANNOT_PLAY_ITEM_CARDS_MARKER, this);
-        }
-        if (effect instanceof play_card_effects_1.PlayItemEffect) {
-            const player = effect.player;
-            if (player.marker.hasMarker(this.OPPONENT_CANNOT_PLAY_ITEM_CARDS_MARKER, this)) {
-                throw new game_1.GameError(game_1.GameMessage.BLOCKED_BY_EFFECT);
+            if (opponent.active.getPokemonCard() === this) {
+                throw new game_1.GameError(game_1.GameMessage.CANNOT_PLAY_THIS_CARD);
             }
+        }
+        if (effect instanceof game_effects_1.AttackEffect && effect.attack === this.attacks[0]) {
+            const player = effect.player;
+            const opponent = game_1.StateUtils.getOpponent(state, player);
+            const opponentHasBench = opponent.bench.some(b => b.cards.length > 0);
+            if (!opponentHasBench) {
+                return state;
+            }
+            const max = Math.min(2);
+            return store.prompt(state, new game_1.ChoosePokemonPrompt(player.id, game_1.GameMessage.CHOOSE_POKEMON_TO_DAMAGE, game_1.PlayerType.TOP_PLAYER, [game_1.SlotType.BENCH], { min: 1, max: max, allowCancel: false }), selected => {
+                const targets = selected || [];
+                targets.forEach(target => {
+                    const damageEffect = new attack_effects_1.PutDamageEffect(effect, 20);
+                    damageEffect.target = target;
+                    store.reduceEffect(state, damageEffect);
+                });
+                return state;
+            });
         }
         return state;
     }
