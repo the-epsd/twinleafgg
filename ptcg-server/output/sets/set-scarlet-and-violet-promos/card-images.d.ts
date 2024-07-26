0b42820bc1a92a4920bcdbabfc36d485ffdc008a
RawrJoey <joey.myerscough@gmail.com>
2024-07-26 02:57:28 -0400

    sylveon + ceruledge

--- ptcg-server/output/sets/set-scarlet-and-violet-promos/card-images.d.ts
+++ ptcg-server/output/sets/set-scarlet-and-violet-promos/card-images.d.ts
@@ -1,16 +1,32 @@
 import { Ampharosex } from './ampharos-ex';
 import { Annihilapeex } from './annihilape-ex';
+import { Ceruledgeex } from './ceruledge-ex';
+import { Charcadet } from './charcadet';
 import { Charmander } from './charmander';
+import { Eevee } from './eevee';
 import { Mimikyuex } from './mimikyu-ex';
+import { Sylveonex } from './sylveon-ex';
 export declare class AmpharosexArt extends Ampharosex {
     cardImage: string;
 }
 export declare class AnnihilapeexArt extends Annihilapeex {
     cardImage: string;
 }
+export declare class CeruledgeexArt extends Ceruledgeex {
+    cardImage: string;
+}
+export declare class CharcadetArt extends Charcadet {
+    cardImage: string;
+}
 export declare class CharmanderArt extends Charmander {
     cardImage: string;
 }
+export declare class EeveeArt extends Eevee {
+    cardImage: string;
+}
 export declare class MimikyuexArt extends Mimikyuex {
+    cardImage: string;
+}
+export declare class SylveonexArt extends Sylveonex {
     cardImage: string;
 }
--- ptcg-server/src/sets/set-scarlet-and-violet-promos/card-images.ts
+++ ptcg-server/src/sets/set-scarlet-and-violet-promos/card-images.ts
@@ -1,7 +1,11 @@
 import { Ampharosex } from './ampharos-ex';
 import { Annihilapeex } from './annihilape-ex';
+import { Ceruledgeex } from './ceruledge-ex';
+import { Charcadet } from './charcadet';
 import { Charmander } from './charmander';
+import { Eevee } from './eevee';
 import { Mimikyuex } from './mimikyu-ex';
+import { Sylveonex } from './sylveon-ex';
 
 export class AmpharosexArt extends Ampharosex {
   public cardImage = 'https://limitlesstcg.nyc3.digitaloceanspaces.com/tpci/SVP/SVP_016_R_EN_LG.png';
@@ -11,10 +15,26 @@ export class AnnihilapeexArt extends Annihilapeex {
   public cardImage = 'https://limitlesstcg.nyc3.digitaloceanspaces.com/tpci/SVP/SVP_032_R_EN_LG.png';
 }
 
+export class CeruledgeexArt extends Ceruledgeex {
+  public cardImage = 'https://images.squarespace-cdn.com/content/v1/5cf4cfa4382ac0000123aa1b/1721900585098-11M9Z2XFZGYMILPNRN8H/Ceruledge+ex.png?format=1500w';
+}
+
+export class CharcadetArt extends Charcadet {
+  public cardImage = 'https://images.squarespace-cdn.com/content/v1/5cf4cfa4382ac0000123aa1b/1721900292709-J44T566DBM8GDUUQWXB5/Charcadet.png?format=1500w';
+}
+
 export class CharmanderArt extends Charmander {
   public cardImage = 'https://limitlesstcg.nyc3.digitaloceanspaces.com/tpci/SVP/SVP_047_R_EN_LG.png';
 }
 
+export class EeveeArt extends Eevee {
+  public cardImage = 'https://images.squarespace-cdn.com/content/v1/5cf4cfa4382ac0000123aa1b/1721900292627-Z7DJUJWZJHZ4525SHSNE/Eevee.png?format=1500w';
+}
+
 export class MimikyuexArt extends Mimikyuex {
   public cardImage = 'https://limitlesstcg.nyc3.digitaloceanspaces.com/tpci/SVP/SVP_004_R_EN_LG.png';
+}
+
+export class SylveonexArt extends Sylveonex {
+  public cardImage = 'https://images.squarespace-cdn.com/content/v1/5cf4cfa4382ac0000123aa1b/1721958763940-NX5527U2FDTBX1PCAPGO/Sylveon+ex.png?format=1500w';
 }
