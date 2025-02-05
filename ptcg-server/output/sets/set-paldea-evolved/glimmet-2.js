d46652410e7f15368ff95b9cebf20d30d79c3856
Big Log <thehighclassmareeper@gmail.com>
2025-02-05 11:04:32 -0500

    PAL glimmet added

---
+++ ptcg-server/output/sets/set-paldea-evolved/glimmet-2.js
@@ -0,0 +1,59 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Glimmet2 = void 0;
+const pokemon_card_1 = require("../../game/store/card/pokemon-card");
+const card_types_1 = require("../../game/store/card/card-types");
+const game_effects_1 = require("../../game/store/effects/game-effects");
+const game_message_1 = require("../../game/game-message");
+const choose_cards_prompt_1 = require("../../game/store/prompts/choose-cards-prompt");
+const shuffle_prompt_1 = require("../../game/store/prompts/shuffle-prompt");
+function* useAscension(next, store, state, effect) {
+    const player = effect.player;
+    if (player.deck.cards.length === 0) {
+        return state;
+    }
+    let cards = [];
+    yield store.prompt(state, new choose_cards_prompt_1.ChooseCardsPrompt(player, game_message_1.GameMessage.CHOOSE_CARD_TO_EVOLVE, player.deck, { superType: card_types_1.SuperType.POKEMON, stage: card_types_1.Stage.STAGE_1, evolvesFrom: 'Glimmet' }, { min: 1, max: 1, allowCancel: true }), selected => {
+        cards = selected || [];
+        next();
+    });
+    if (cards.length > 0) {
+        // Evolve Pokemon
+        player.deck.moveCardsTo(cards, player.active);
+        player.active.clearEffects();
+        player.active.pokemonPlayedTurn = state.turn;
+    }
+    return store.prompt(state, new shuffle_prompt_1.ShuffleDeckPrompt(player.id), order => {
+        player.deck.applyOrder(order);
+    });
+}
+class Glimmet2 extends pokemon_card_1.PokemonCard {
+    constructor() {
+        super(...arguments);
+        this.stage = card_types_1.Stage.BASIC;
+        this.cardType = F;
+        this.hp = 40;
+        this.weakness = [{ type: G }];
+        this.retreat = [C];
+        this.attacks = [{
+                name: 'Ascension',
+                cost: [F],
+                damage: 0,
+                text: 'Search your deck for a card that evolves from this Pokémon and put it onto this Pokémon to evolve it. Then, shuffle your deck.'
+            }];
+        this.set = 'PAL';
+        this.regulationMark = 'G';
+        this.cardImage = 'assets/cardback.png';
+        this.setNumber = '124';
+        this.name = 'Glimmet';
+        this.fullName = 'Glimmet PAL';
+    }
+    reduceEffect(store, state, effect) {
+        if (effect instanceof game_effects_1.AttackEffect && effect.attack === this.attacks[0]) {
+            const generator = useAscension(() => generator.next(), store, state, effect);
+            return generator.next().value;
+        }
+        return state;
+    }
+}
+exports.Glimmet2 = Glimmet2;
