71c38e818574de75ea8aaced2f536696afe6feeb
saavanverma <saavanverma@gmail.com>
2025-03-09 03:26:50 -0400

    Added Azelf LA

---
+++ ptcg-server/output/sets/set-legends-awakened/azelf.d.ts
@@ -0,0 +1,32 @@
+import { Effect } from '../../game/store/effects/effect';
+import { PokemonCard } from '../../game/store/card/pokemon-card';
+import { Stage, CardType } from '../../game/store/card/card-types';
+import { PowerType, StoreLike, State } from '../../game';
+export declare class Azelf extends PokemonCard {
+    stage: Stage;
+    cardType: CardType;
+    hp: number;
+    weakness: {
+        type: CardType;
+        value: number;
+    }[];
+    retreat: CardType[];
+    powers: {
+        name: string;
+        powerType: PowerType;
+        text: string;
+    }[];
+    attacks: {
+        name: string;
+        cost: CardType[];
+        damage: number;
+        text: string;
+    }[];
+    set: string;
+    setNumber: string;
+    cardImage: string;
+    name: string;
+    fullName: string;
+    readonly DEFENDING_POKEMON_CANNOT_RETREAT_MARKER = "DEFENDING_POKEMON_CANNOT_RETREAT_MARKER";
+    reduceEffect(store: StoreLike, state: State, effect: Effect): State;
+}
--- ptcg-server/src/sets/set-legends-awakened/index.ts
+++ ptcg-server/src/sets/set-legends-awakened/index.ts
@@ -1,9 +1,11 @@
 import { Card } from '../../game/store/card/card';
 import { UnownR } from './unown-r';
 import { Uxie } from './uxie';
+import { Azelf } from './azelf';
 
 export const setLegendsAwakened: Card[] = [
 
   new UnownR(),
   new Uxie(),
+  new Azelf(),
 ];
