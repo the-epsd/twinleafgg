49d60ec8dff6f0a9424958b48fb217100445fe94
saavanverma <saavanverma@gmail.com>
2025-03-09 13:49:17 -0400

    Added Baltoy and Claydol from GE

--- ptcg-server/output/sets/set-great-encounters/index.js
+++ ptcg-server/output/sets/set-great-encounters/index.js
@@ -1,9 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.setGreatEncounters = void 0;
+const baltoy_1 = require("./baltoy");
 const buizel_1 = require("./buizel");
+const claydol_1 = require("./claydol");
 const floatzel_1 = require("./floatzel");
 exports.setGreatEncounters = [
     new buizel_1.Buizel(),
     new floatzel_1.Floatzel(),
+    new baltoy_1.Baltoy(),
+    new claydol_1.Claydol()
 ];
