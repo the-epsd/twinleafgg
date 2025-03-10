1822e50721c71474a6f06ccf354e908eb38d9ab5
saavanverma <saavanverma@gmail.com>
2025-03-10 12:40:11 -0400

    Added Pidgeot & Pidgeotto LF and fixed Pidgeot ex

--- ptcg-server/src/sets/set-ex-firered-leafgreen/index.ts
+++ ptcg-server/src/sets/set-ex-firered-leafgreen/index.ts
@@ -1,8 +1,12 @@
 import { Card } from '../../game/store/card/card';
 import { VsSeekerFL } from './other-prints';
+import { Pidgeot } from './pidgeot';
+import { Pidgeotto } from './pidgeotto';
 import { Pidgey } from './pidgey';
 
 export const setEXFireRedLeafGreen: Card[] = [
   new Pidgey(),
-  new VsSeekerFL(),
+  new Pidgeotto(),
+  new Pidgeot(),
+  new VsSeekerFL()
 ];
