d063e913c621982d62b26cc3965ed6da460555ff
Bencbabcock <bencbabcock01@gmail.com>
2025-03-23 07:49:48 -0400

    Added Oddish from Legends Awakened

--- ptcg-server/src/sets/set-legends-awakened/index.ts
+++ ptcg-server/src/sets/set-legends-awakened/index.ts
@@ -2,10 +2,12 @@ import { Card } from '../../game/store/card/card';
 import { UnownR } from './unown-r';
 import { Uxie } from './uxie';
 import { Azelf } from './azelf';
+import { Oddish } from './oddish';
 
 export const setLegendsAwakened: Card[] = [
 
   new UnownR(),
   new Uxie(),
   new Azelf(),
+  new Oddish(),
 ];
