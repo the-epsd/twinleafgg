a6cbdecbc3d94edef5cca480d5f081141474f45a
Bencbabcock <bencbabcock01@gmail.com>
2025-03-23 11:35:26 -0400

    Added Pidgey from Holon Phanotms

--- ptcg-server/src/sets/set-ex-holon-phantoms/index.ts
+++ ptcg-server/src/sets/set-ex-holon-phantoms/index.ts
@@ -1,9 +1,11 @@
 import { Card } from '../../game/store/card/card';
-import {HolonsCastform} from './holons-castform';
+import { HolonsCastform } from './holons-castform';
+import { Pidgey } from './pidgey';
 import { Pikachu } from './pikachu';
 import { Raichu } from './raichu';
 
 export const setEXHolonPhantoms: Card[] = [
+  new Pidgey(),
   new Pikachu(),
   new Raichu(),
   new HolonsCastform(),
