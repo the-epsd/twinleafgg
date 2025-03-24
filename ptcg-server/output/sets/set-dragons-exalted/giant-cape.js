bfd1a1b56ff879bfbce10c6bb5e84bd4e0c3586b
Big Log <thehighclassmareeper@gmail.com>
2025-03-24 19:26:21 -0400

    a little addition

--- ptcg-server/output/sets/set-dragons-exalted/giant-cape.js
+++ ptcg-server/output/sets/set-dragons-exalted/giant-cape.js
@@ -1,31 +1,29 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.HerosCape = void 0;
+exports.GiantCape = void 0;
 const trainer_card_1 = require("../../game/store/card/trainer-card");
 const card_types_1 = require("../../game/store/card/card-types");
 const check_effects_1 = require("../../game/store/effects/check-effects");
 const prefabs_1 = require("../../game/store/prefabs/prefabs");
-class HerosCape extends trainer_card_1.TrainerCard {
+class GiantCape extends trainer_card_1.TrainerCard {
     constructor() {
         super(...arguments);
         this.trainerType = card_types_1.TrainerType.TOOL;
-        this.tags = [card_types_1.CardTag.ACE_SPEC];
-        this.regulationMark = 'H';
-        this.set = 'TEF';
+        this.set = 'DRX';
+        this.name = 'Giant Cape';
+        this.fullName = 'Giant Cape DRX';
         this.cardImage = 'assets/cardback.png';
-        this.setNumber = '152';
-        this.name = 'Hero\'s Cape';
-        this.fullName = 'Hero\'s Cape TEF';
-        this.text = 'The Pokémon this card is attached to gets +100 HP.';
+        this.setNumber = '114';
+        this.text = 'The Pokémon this card is attached to gets +20 HP.';
     }
     reduceEffect(store, state, effect) {
         if (effect instanceof check_effects_1.CheckHpEffect && effect.target.cards.includes(this)) {
             if (prefabs_1.IS_TOOL_BLOCKED(store, state, effect.player, this)) {
                 return state;
             }
-            effect.hp += 100;
+            effect.hp += 20;
         }
         return state;
     }
 }
-exports.HerosCape = HerosCape;
+exports.GiantCape = GiantCape;
--- ptcg-server/src/sets/set-dragons-exalted/giant-cape.ts
+++ ptcg-server/src/sets/set-dragons-exalted/giant-cape.ts
@@ -1,41 +1,29 @@
 import { TrainerCard } from '../../game/store/card/trainer-card';
-import { CardTag, TrainerType } from '../../game/store/card/card-types';
+import { TrainerType } from '../../game/store/card/card-types';
 import { StoreLike } from '../../game/store/store-like';
 import { State } from '../../game/store/state/state';
 import { Effect } from '../../game/store/effects/effect';
 import { CheckHpEffect } from '../../game/store/effects/check-effects';
-import {IS_TOOL_BLOCKED} from '../../game/store/prefabs/prefabs';
+import { IS_TOOL_BLOCKED } from '../../game/store/prefabs/prefabs';
 
-
-export class HerosCape extends TrainerCard {
-
+export class GiantCape extends TrainerCard {
   public trainerType: TrainerType = TrainerType.TOOL;
-
-  public tags = [CardTag.ACE_SPEC];
-
-  public regulationMark = 'H';
-
-  public set: string = 'TEF';
-
+  public set: string = 'DRX';
+  public name: string = 'Giant Cape';
+  public fullName: string = 'Giant Cape DRX';
   public cardImage: string = 'assets/cardback.png';
-
-  public setNumber: string = '152';
-
-  public name: string = 'Hero\'s Cape';
-
-  public fullName: string = 'Hero\'s Cape TEF';
+  public setNumber: string = '114';
 
   public text: string =
-    'The Pokémon this card is attached to gets +100 HP.';
+    'The Pokémon this card is attached to gets +20 HP.';
 
   public reduceEffect(store: StoreLike, state: State, effect: Effect): State {
-
-    if (effect instanceof CheckHpEffect && effect.target.cards.includes(this)) {
-
+    if (effect instanceof CheckHpEffect && effect.target.cards.includes(this)){
       if (IS_TOOL_BLOCKED(store, state, effect.player, this)){ return state; }
 
-      effect.hp += 100;
+      effect.hp += 20;
     }
     return state;
   }
+
 }
