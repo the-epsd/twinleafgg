38759e7f284590ea10e18257648f7a4ac3c99dae
Bencbabcock <bencbabcock01@gmail.com>
2025-03-23 21:49:39 -0400

    Added Oddish from Holon Phantoms

--- ptcg-server/output/sets/set-ex-holon-phantoms/index.js
+++ ptcg-server/output/sets/set-ex-holon-phantoms/index.js
@@ -2,9 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.setEXHolonPhantoms = void 0;
 const holons_castform_1 = require("./holons-castform");
+const oddish_1 = require("./oddish");
 const pikachu_1 = require("./pikachu");
 const raichu_1 = require("./raichu");
 exports.setEXHolonPhantoms = [
+    new oddish_1.Oddish(),
     new pikachu_1.Pikachu(),
     new raichu_1.Raichu(),
     new holons_castform_1.HolonsCastform(),
--- ptcg-server/src/sets/set-ex-holon-phantoms/index.ts
+++ ptcg-server/src/sets/set-ex-holon-phantoms/index.ts
@@ -1,9 +1,11 @@
 import { Card } from '../../game/store/card/card';
-import {HolonsCastform} from './holons-castform';
+import { HolonsCastform } from './holons-castform';
+import { Oddish } from './oddish';
 import { Pikachu } from './pikachu';
 import { Raichu } from './raichu';
 
 export const setEXHolonPhantoms: Card[] = [
+  new Oddish(),
   new Pikachu(),
   new Raichu(),
   new HolonsCastform(),
