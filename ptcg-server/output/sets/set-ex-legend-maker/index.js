f5534bccb61a808720f9058d263f4bcaac0a3bac
Bencbabcock <bencbabcock01@gmail.com>
2025-03-26 12:10:38 -0400

    Added React Energy from Legend Maker

--- ptcg-server/output/sets/set-ex-legend-maker/index.js
+++ ptcg-server/output/sets/set-ex-legend-maker/index.js
@@ -3,7 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.setEXLegendMaker = void 0;
 const lapras_1 = require("./lapras");
 const pikachu_1 = require("./pikachu");
+const react_energy_1 = require("./react-energy");
 exports.setEXLegendMaker = [
     new lapras_1.Lapras(),
     new pikachu_1.Pikachu(),
+    new react_energy_1.ReactEnergy(),
 ];
--- ptcg-server/src/sets/set-ex-legend-maker/index.ts
+++ ptcg-server/src/sets/set-ex-legend-maker/index.ts
@@ -1,8 +1,10 @@
 import { Card } from '../../game';
 import { Lapras } from './lapras';
 import { Pikachu } from './pikachu';
+import { ReactEnergy } from './react-energy';
 
 export const setEXLegendMaker: Card[] = [
   new Lapras(),
   new Pikachu(),
+  new ReactEnergy(),
 ];
