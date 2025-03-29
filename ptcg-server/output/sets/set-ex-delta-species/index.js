567dda09d6ecf6672b5bdf5ba1df814a1c85f1eb
Bencbabcock <bencbabcock01@gmail.com>
2025-03-29 07:58:32 -0400

    Added Holon's Electrode and Voltorb from DS

--- ptcg-server/output/sets/set-ex-delta-species/index.js
+++ ptcg-server/output/sets/set-ex-delta-species/index.js
@@ -1,7 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.setEXDeltaSpecies = void 0;
+const holons_electrode_1 = require("./holons-electrode");
+const holons_voltorb_1 = require("./holons-voltorb");
 const other_prints_1 = require("./other-prints");
 exports.setEXDeltaSpecies = [
+    new holons_electrode_1.HolonsElectrode(),
+    new holons_voltorb_1.HolonsVoltorb(),
     new other_prints_1.SuperScoopUpDS(),
 ];
