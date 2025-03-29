355e2fa1a764e3e36f97ecb49350349991803e96
Bencbabcock <bencbabcock01@gmail.com>
2025-03-29 08:55:02 -0400

    Added Holon's Magneton and Magnemite from DS

--- ptcg-server/output/sets/set-ex-delta-species/index.js
+++ ptcg-server/output/sets/set-ex-delta-species/index.js
@@ -1,7 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.setEXDeltaSpecies = void 0;
+const holons_magnemite_1 = require("./holons-magnemite");
+const holons_magneton_1 = require("./holons-magneton");
 const other_prints_1 = require("./other-prints");
 exports.setEXDeltaSpecies = [
+    new holons_magnemite_1.HolonsMagnemite(),
+    new holons_magneton_1.HolonsMagneton(),
     new other_prints_1.SuperScoopUpDS(),
 ];
