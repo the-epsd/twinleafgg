08b725d40757cc313af1be551a0b3e24f61c55da
Bencbabcock <bencbabcock01@gmail.com>
2025-04-06 09:14:57 -0400

    Indexed React Energy LM

--- ptcg-server/src/sets/set-ex-legend-maker/index.ts
+++ ptcg-server/src/sets/set-ex-legend-maker/index.ts
@@ -3,10 +3,12 @@ import { GiantStump } from './giant-stump';
 import { Kecleon } from './kecleon';
 import { Lapras } from './lapras';
 import { Pikachu } from './pikachu';
+import { ReactEnergy } from './react-energy';
 
 export const setEXLegendMaker: Card[] = [
   new GiantStump(),
   new Kecleon(),
   new Lapras(),
   new Pikachu(),
+  new ReactEnergy(),
 ];
