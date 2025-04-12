ee4cf1394266106f256bfa2afba527ddcea84f62
Bencbabcock <bencbabcock01@gmail.com>
2025-04-12 16:29:47 -0400

    Added Cleffa DP

--- ptcg-server/src/sets/set-diamond-and-pearl/index.ts
+++ ptcg-server/src/sets/set-diamond-and-pearl/index.ts
@@ -1,10 +1,12 @@
 import { Card } from '../../game/store/card/card';
+import { Cleffa } from './cleffa';
 import { Meditite } from './meditite';
 import { PokedexHandy } from './pokedex-handy';
 import { SuperScoopUp } from './super-scoop-up';
 import { Wynaut } from './wynaut';
 
 export const setDiamondAndPearl: Card[] = [
+  new Cleffa(),
   new Meditite(),
   new PokedexHandy(),
   new SuperScoopUp(),
