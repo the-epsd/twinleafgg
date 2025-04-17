05fed4a95d1b7925fb5dc80b3b77e82cd56e8a37
Benjamin Babcock <bencbabcock01@gmail.com>
2025-04-17 07:11:51 -0400

    Added Flareon Star PK

--- ptcg-server/src/game/store/card/card-types.ts
+++ ptcg-server/src/game/store/card/card-types.ts
@@ -23,6 +23,7 @@ export enum CardTag {
   TAG_TEAM = 'TAG_TEAM',
   TEAM_MAGMA = 'TEAM_MAGMA',
   PRISM_STAR = 'PRISM_STAR',
+  STAR = 'STAR',
   BREAK = 'BREAK',
   PRIME = 'PRIME',
   HOLO = 'HOLO',
