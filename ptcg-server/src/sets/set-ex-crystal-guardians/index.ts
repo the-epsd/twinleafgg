d4d337ce52d172fb542ddd739dfa00b15fc324ec
Benjamin Babcock <bencbabcock01@gmail.com>
2025-05-01 10:04:25 -0400

    Added Alakazam Star CG

--- ptcg-server/src/sets/set-ex-crystal-guardians/index.ts
+++ ptcg-server/src/sets/set-ex-crystal-guardians/index.ts
@@ -1,4 +1,5 @@
 import { Card } from '../../game';
+import { AlakazamStar } from './alakazam-star';
 import { Castaway } from './castaway';
 import { CessationCrystal } from './cessation-crystal';
 import { Charmander } from './charmander';
@@ -7,6 +8,7 @@ import { Dugtrio } from './dugtrio';
 import { Treecko } from './treecko';
 
 export const setEXCrystalGuardians: Card[] = [
+  new AlakazamStar(),
   new Castaway(),
   new CessationCrystal(),
   new Charmander(),
