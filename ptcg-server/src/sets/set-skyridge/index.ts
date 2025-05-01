5ca2077eaa987ab00bcc6fc3e5cec8e50fd426c7
Benjamin Babcock <bencbabcock01@gmail.com>
2025-05-01 17:18:49 -0400

    Added Ditto SK

--- ptcg-server/src/sets/set-skyridge/index.ts
+++ ptcg-server/src/sets/set-skyridge/index.ts
@@ -1,8 +1,10 @@
 import { Card } from '../../game/store/card/card';
 import { BuriedFossil } from './buried-fossil';
+import { Ditto } from './ditto';
 import { Kabuto } from './kabuto';
 
 export const setSkyridge: Card[] = [
   new BuriedFossil(),
+  new Ditto(),
   new Kabuto(),
 ];
