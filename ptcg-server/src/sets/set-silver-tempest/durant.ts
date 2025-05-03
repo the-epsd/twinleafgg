f635023887c63919bf902f504f8f3ddbb4c0f2f0
Cealgair <sr11.divisozero@gmail.com>
2025-05-03 18:00:41 +0200

    Added Durant SIT

---
+++ ptcg-server/src/sets/set-silver-tempest/durant.ts
@@ -0,0 +1,81 @@
+import { PokemonCard } from '../../game/store/card/pokemon-card';
+import { Stage, CardType, SuperType, EnergyType } from '../../game/store/card/card-types';
+import { Card, ChooseCardsPrompt, ShuffleDeckPrompt } from '../../game';
+import { StoreLike, State, GameMessage } from '../../game';
+import { Effect } from '../../game/store/effects/effect';
+import { AttackEffect } from '../../game/store/effects/game-effects';
+import {SHOW_CARDS_TO_PLAYER} from '../../game/store/prefabs/prefabs';
+
+
+export class Durant extends PokemonCard {
+
+  public stage: Stage = Stage.BASIC;
+
+  public cardType: CardType = CardType.GRASS;
+
+  public hp: number = 110;
+
+  public weakness = [{ type: CardType.FIRE }];
+
+  public retreat = [ CardType.COLORLESS, CardType.COLORLESS ];
+
+  public attacks = [
+    {
+      name: 'Energy Digging',
+      cost: [ CardType.COLORLESS ],
+      damage: 0,
+      text: 'Search your deck for up to 2 basic Energy cards, reveal them, and put them into your hand. Then, shuffle your deck.'
+    },
+    {
+      name: 'Bite',
+      cost: [ CardType.GRASS, CardType.COLORLESS ],
+      damage: 50,
+      text: ''
+    }
+  ];
+
+  public regulationMark = 'F';
+
+  public set: string = 'SIT';
+
+  public cardImage: string = 'assets/cardback.png';
+
+  public setNumber: string = '13';
+
+  public name: string = 'Durant';
+
+  public fullName: string = 'Durant SIT';
+
+  public reduceEffect(store: StoreLike, state: State, effect: Effect): State {
+    // Copied from Gimmighoul SSP's Minor Errand-Running
+    if (effect instanceof AttackEffect && effect.attack === this.attacks[0]) {
+      const player = effect.player;
+      const opponent = effect.opponent;
+
+      if (player.deck.cards.length === 0) {
+        return state;
+      }
+
+      let cards: Card[] = [];
+      store.prompt(state, new ChooseCardsPrompt(
+        player,
+        GameMessage.CHOOSE_CARD_TO_HAND,
+        player.deck,
+        { superType: SuperType.ENERGY, energyType: EnergyType.BASIC },
+        { min: 0, max: 2, allowCancel: false }
+      ), selected => {
+        cards = selected || [];
+
+        SHOW_CARDS_TO_PLAYER(store, state, opponent, cards);
+
+        player.deck.moveCardsTo(cards, player.hand);
+      });
+
+      return store.prompt(state, new ShuffleDeckPrompt(player.id), order => {
+        player.deck.applyOrder(order);
+      });
+    }
+
+    return state;
+  }
+}
