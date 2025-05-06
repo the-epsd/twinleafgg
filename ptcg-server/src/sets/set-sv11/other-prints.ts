d54896cb0f45d02c0e2fd2689da6b648be91ba36
Joe Myerscough <joey.myerscough@gmail.com>
2025-05-05 23:34:11 -0400

    SV11 Update

---
+++ ptcg-server/src/sets/set-sv11/other-prints.ts
@@ -0,0 +1,40 @@
+import { Cheren } from "../set-emerging-powers/cheren";
+import { PrismEnergy } from "../set-next-destinies/prism-energy";
+import { EnergyRetrieval } from "../set-scarlet-and-violet/energy-retrieval";
+import { ProfessorsResearch } from "../set-scarlet-and-violet/professors-research";
+import { AirBalloon } from "../set-sword-and-shield/air-balloon";
+
+export class AirBalloonSV11 extends AirBalloon {
+  public regulationMark = 'I';
+  public setNumber = '82';
+  public fullName: string = 'Air Balloon SV11B';
+  public set = 'SV11B';
+}
+
+export class PrismEnergySV11 extends PrismEnergy {
+  public regulationMark = 'I';
+  public setNumber = '86';
+  public fullName: string = 'Prism Energy SV11B';
+  public set = 'SV11B';
+}
+
+export class CherenSV11 extends Cheren {
+  public regulationMark = 'I';
+  public setNumber = '84';
+  public fullName: string = 'Cheren SV11W';
+  public set = 'SV11W';
+}
+
+export class ProfessorsResearchSV11 extends ProfessorsResearch {
+  public regulationMark = 'G';
+  public setNumber = '84';
+  public fullName: string = 'Professors Research SV11B';
+  public set = 'SV11B';
+}
+
+export class EnergyRetrievalSV11 extends EnergyRetrieval {
+  public regulationMark = 'I';
+  public setNumber = '79';
+  public fullName: string = 'Energy Retrieval SV11W';
+  public set = 'SV11W';
+}
