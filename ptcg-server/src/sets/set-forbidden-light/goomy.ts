2c54ff83b7cdf22dec020faa3cd68bc2271778bf
Hoo-Knows <73412292+Hoo-Knows@users.noreply.github.com>
2025-05-13 16:35:01 -0700

    Add Goomy FLI 91, Goomy FLI 92, Sliggoo FLI 93, Goodra FLI 94

---
+++ ptcg-server/src/sets/set-forbidden-light/goomy.ts
@@ -0,0 +1,46 @@
+import { PokemonCard } from '../../game/store/card/pokemon-card';
+import { Stage, CardType } from '../../game/store/card/card-types';
+import { PowerType, State, StateUtils, StoreLike } from '../../game';
+import { Effect } from '../../game/store/effects/effect';
+import { CheckAttackCostEffect } from '../../game/store/effects/check-effects';
+import { IS_ABILITY_BLOCKED } from '../../game/store/prefabs/prefabs';
+
+export class Goomy extends PokemonCard {
+  public stage: Stage = Stage.BASIC;
+  public cardType: CardType = N;
+  public hp: number = 40;
+  public weakness = [{ type: Y }];
+  public retreat = [C];
+
+  public powers = [{
+    name: 'Sticky Membrane',
+    text: 'As long as this Pokémon is your Active Pokémon, your opponent\'s Pokémon\'s attacks cost [C] more.',
+    powerType: PowerType.ABILITY
+  }];
+
+  public attacks = [{
+    name: 'Ram',
+    cost: [Y],
+    damage: 10,
+    text: ''
+  }];
+
+  public set: string = 'FLI';
+  public name: string = 'Goomy';
+  public fullName: string = 'Goomy FLI 91';
+  public cardImage: string = 'assets/cardback.png';
+  public setNumber: string = '91';
+
+  public reduceEffect(store: StoreLike, state: State, effect: Effect): State {
+
+    // Sticky Membrane
+    if (effect instanceof CheckAttackCostEffect) {
+      const opponent = StateUtils.getOpponent(state, effect.player);
+      if (opponent.active.getPokemonCard() === this && !IS_ABILITY_BLOCKED(store, state, opponent, this)) {
+        effect.cost.push(CardType.COLORLESS);
+      }
+    }
+
+    return state;
+  }
+}
