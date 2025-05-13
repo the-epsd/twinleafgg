66d9e4b3b5ed2eb72014aa3dc9c814ed6c65f78b
Hoo-Knows <73412292+Hoo-Knows@users.noreply.github.com>
2025-05-13 16:40:44 -0700

    Add Stealthy Hood check

--- ptcg-server/src/sets/set-forbidden-light/goomy.ts
+++ ptcg-server/src/sets/set-forbidden-light/goomy.ts
@@ -4,6 +4,7 @@ import { PowerType, State, StateUtils, StoreLike } from '../../game';
 import { Effect } from '../../game/store/effects/effect';
 import { CheckAttackCostEffect } from '../../game/store/effects/check-effects';
 import { IS_ABILITY_BLOCKED } from '../../game/store/prefabs/prefabs';
+import { EffectOfAbilityEffect } from '../../game/store/effects/game-effects';
 
 export class Goomy extends PokemonCard {
   public stage: Stage = Stage.BASIC;
@@ -37,6 +38,12 @@ export class Goomy extends PokemonCard {
     if (effect instanceof CheckAttackCostEffect) {
       const opponent = StateUtils.getOpponent(state, effect.player);
       if (opponent.active.getPokemonCard() === this && !IS_ABILITY_BLOCKED(store, state, opponent, this)) {
+        const canApplyAbility = new EffectOfAbilityEffect(opponent, this.powers[0], this, effect.player.active);
+        store.reduceEffect(state, canApplyAbility);
+        if (!canApplyAbility.target) {
+          return state;
+        }
+
         effect.cost.push(CardType.COLORLESS);
       }
     }
