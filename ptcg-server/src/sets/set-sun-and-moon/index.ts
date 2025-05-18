7a183a026a3cea669598be5865d965114fe3f051
Benjamin Babcock <bencbabcock01@gmail.com>
2025-05-18 10:37:18 -0400

    Added Vikavolt line SUM

--- ptcg-server/src/sets/set-sun-and-moon/index.ts
+++ ptcg-server/src/sets/set-sun-and-moon/index.ts
@@ -4,12 +4,14 @@ import { AlolanGrimer } from './alolan-grimer';
 import { AlolanMuk } from './alolan-muk';
 import { AlolanRattata } from './alolan_rattata';
 import { EnergyRetrievalSUM, ExpShareSUM, NestBallSUM, RareCandySUM, RotomDexSR } from './other-prints';
+import { Charjabug } from './charjabug';
 import { DecidueyeGX } from './decidueye-gx';
 import { Dragonair } from './dragonair';
 import { Eevee } from './eevee';
 import { EspeonGX } from './espeon-gx';
 import { Fomantis } from './fomantis';
 import { Golduck } from './golduck';
+import { Grubbin } from './grubbin';
 import { Herdier } from './herdier';
 import { LurantisGX } from './lurantis-gx';
 import { Oranguru } from './oranguru';
@@ -21,12 +23,14 @@ import { RotomDex } from './rotom-dex';
 import { Rowlet } from './rowlet';
 import { TeamSkullGrunt } from './team-skull-grunt';
 import { TimerBall } from './timer-ball';
-import {TaurosGX} from './tauros-gx';
+import { TaurosGX } from './tauros-gx';
+import { Vikavolt } from './vikavolt';
 
 export const setSunAndMoon: Card[] = [
   new AlolanGrimer(),
   new AlolanMuk(),
   new AlolanRattata(),
+  new Charjabug(),
   new Dartrix(),
   new DecidueyeGX(),
   new Dragonair(),
@@ -34,6 +38,7 @@ export const setSunAndMoon: Card[] = [
   new EspeonGX(),
   new Fomantis(),
   new Golduck(),
+  new Grubbin(),
   new Herdier(),
   new LurantisGX(),
   new Oranguru(),
@@ -46,6 +51,7 @@ export const setSunAndMoon: Card[] = [
   new TeamSkullGrunt(),
   new TimerBall(),
   new TaurosGX(),
+  new Vikavolt(),
 
   // Reprints
   new NestBallSUM(),
