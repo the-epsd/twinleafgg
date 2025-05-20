a261732fe495056130aa6f10abfb5303a49d3e1f
Benjamin Babcock <bencbabcock01@gmail.com>
2025-05-20 11:04:13 -0400

    Added Dark Tyranitar line TRR

--- ptcg-server/src/sets/set-ex-team-rocket-returns/index.ts
+++ ptcg-server/src/sets/set-ex-team-rocket-returns/index.ts
@@ -1,6 +1,10 @@
 import { Card } from '../../game';
 import { DarkAmpharos } from './dark-ampharos';
 import { DarkFlaaffy } from './dark-flaaffy';
+import { DarkPupitar } from './dark-pupitar';
+import { DarkTyranitar } from './dark-tyranitar';
+import { DarkTyranitar2 } from './dark-tyrantiar-2';
+import { Larvitar } from './larvitar';
 import { PokemonRetriever } from './pokemon-retriever';
 import { PowHandExtension } from './pow-hand-extension';
 import { RocketsAdmin } from './rockets-admin';
@@ -9,6 +13,10 @@ import { SurpriseTimeMachine } from './surprise-time-machine';
 export const setEXTeamRocketReturns: Card[] = [
   new DarkAmpharos(),
   new DarkFlaaffy(),
+  new DarkPupitar(),
+  new DarkTyranitar(),
+  new DarkTyranitar2(),
+  new Larvitar(),
   new PokemonRetriever(),
   new PowHandExtension(),
   new RocketsAdmin(),
