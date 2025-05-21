dd029b8ab7fb120515f9785e23a959c60486f352
Benjamin Babcock <bencbabcock01@gmail.com>
2025-05-21 12:46:04 -0400

    Added Metal and Rainbow Energy prints RS

--- ptcg-server/src/sets/set-ex-ruby-and-sapphire/index.ts
+++ ptcg-server/src/sets/set-ex-ruby-and-sapphire/index.ts
@@ -3,8 +3,16 @@ import { Delcatty } from './delcatty';
 import { Skitty } from './skitty';
 import { Blaziken } from './blaziken';
 
+// Other Prints
+import { MetalEnergySpecialRS } from './other-prints';
+import { RainbowEnergyRS } from './other-prints';
+
 export const setEXRubyAndSapphire: Card[] = [
   new Delcatty(),
   new Skitty(),
-  new Blaziken()
+  new Blaziken(),
+
+  // Other Prints
+  new MetalEnergySpecialRS(),
+  new RainbowEnergyRS(),
 ]; 
