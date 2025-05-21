10409510f3a3c917b7d036d7dcf3dffa5af33044
Benjamin Babcock <bencbabcock01@gmail.com>
2025-05-20 20:08:35 -0400

    Added Multi Energy SS

--- ptcg-server/src/sets/set-ex-sandstorm/index.ts
+++ ptcg-server/src/sets/set-ex-sandstorm/index.ts
@@ -1,12 +1,13 @@
 import { Card } from '../../game/store/card/card';
 import { Dunsparce } from './dunsparce';
 import { Lombre } from './lombre';
+import { MultiEnergy } from './multi-energy';
 import { Wobbuffet } from './wobbuffet';
 
 
 export const setEXSandstorm: Card[] = [
   new Dunsparce(),
   new Lombre(),
+  new MultiEnergy(),
   new Wobbuffet(),
-
 ];
