47d1b285f32eda1743e59487584e3ec994c14b0d
Benjamin Babcock <bencbabcock01@gmail.com>
2025-05-28 12:01:44 -0400

    Added Milotic and Feebas HL

--- ptcg-server/src/sets/set-ex-hidden-legends/index.ts
+++ ptcg-server/src/sets/set-ex-hidden-legends/index.ts
@@ -1,12 +1,16 @@
 import { Card } from '../../game/store/card/card';
 import { DesertRuins } from './desert-ruins';
+import { Feebas } from './feebas';
 import { Jirachi } from './jirachi';
+import { Milotic } from './milotic';
 import { Ninetales } from './ninetales';
 import { Vulpix } from './vulpix';
 
 export const setEXHiddenLegends: Card[] = [
   new DesertRuins(),
+  new Feebas(),
   new Jirachi(),
+  new Milotic(),
   new Ninetales(),
   new Vulpix(),
 ];
