74070d80e59ece432a5a061cfdb63828ab188b69
Benjamin Babcock <bencbabcock01@gmail.com>
2025-05-28 10:28:20 -0400

    Added Nidoqueen line RG

--- ptcg-server/src/sets/set-ex-firered-leafgreen/index.ts
+++ ptcg-server/src/sets/set-ex-firered-leafgreen/index.ts
@@ -2,12 +2,18 @@ import { Card } from '../../game/store/card/card';
 import { Ditto } from './ditto';
 import { Electrodeex } from './electrode-ex';
 import { VsSeekerFL } from './other-prints';
+import { Nidoqueen } from './nidoqueen';
+import { NidoranFemale } from './nidoran-female';
+import { Nidorina } from './nidorina';
 import { Pidgeot } from './pidgeot';
 import { Pidgeotto } from './pidgeotto';
 import { Pidgey } from './pidgey';
 import { Voltorb } from './voltorb';
 
 export const setEXFireRedLeafGreen: Card[] = [
+  new Nidoqueen(),
+  new NidoranFemale(),
+  new Nidorina(),
   new Pidgey(),
   new Pidgeotto(),
   new Pidgeot(),
