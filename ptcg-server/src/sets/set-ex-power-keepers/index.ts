4c5864576dc6e0ab84ceb5886ec68090a4cbb462
Benjamin Babcock <bencbabcock01@gmail.com>
2025-05-27 20:46:34 -0400

    Added Jolteon Star PK

--- ptcg-server/src/sets/set-ex-power-keepers/index.ts
+++ ptcg-server/src/sets/set-ex-power-keepers/index.ts
@@ -2,11 +2,13 @@ import { Card } from '../../game';
 import { GrassEnergy, FireEnergy, WaterEnergy, LightningEnergy, PsychicEnergy, FightingEnergy } from './basic-energies';
 import { EnergyRemoval2 } from './energy-removal-2';
 import { FlareonStar } from './flareon-star';
+import { JolteonStar } from './jolteon-star';
 import { StevensAdvice } from './stevens-advice';
 
 export const setEXPowerKeepers: Card[] = [
   new EnergyRemoval2(),
   new FlareonStar(),
+  new JolteonStar(),
   new StevensAdvice(),
 
   // Basic energies
