1ae47096b23df9ebf70572881ff60ccf1fad7cdc
Benjamin Babcock <bencbabcock01@gmail.com>
2025-05-28 13:14:23 -0400

    Added Nidoqueen DF

--- ptcg-server/src/sets/set-ex-dragon-frontiers/index.ts
+++ ptcg-server/src/sets/set-ex-dragon-frontiers/index.ts
@@ -2,11 +2,13 @@ import { Card } from '../../game';
 import { Copycat } from './copycat';
 import { Mantine } from './mantine';
 import { MrStonesProject } from './mr-stones-project';
+import { Nidoqueen } from './nidoqueen';
 import { Rayquazaex } from './rayquaza-ex';
 
 export const setEXDragonFrontiers: Card[] = [
   new Copycat(),
   new Mantine(),
   new MrStonesProject(),
+  new Nidoqueen(),
   new Rayquazaex()
 ];
