257825bd76eec6d8a989b7cc891d2b01dba4136d
Benjamin Babcock <bencbabcock01@gmail.com>
2025-05-29 06:23:57 -0400

    Added TV Reporter DR

--- ptcg-server/src/sets/set-ex-dragon/index.ts
+++ ptcg-server/src/sets/set-ex-dragon/index.ts
@@ -4,11 +4,13 @@ import { Magneton } from './magneton';
 import { Mareep } from './mareep';
 import { Meditite } from './meditite';
 import { MrBrineysCompassion } from './mr-brineys-compassion';
+import { TvReporter } from './tv-reporter';
 
 export const setEXDragon: Card[] = [
   new Magnemite(),
   new Magneton(),
   new Mareep(),
   new Meditite(),
-  new MrBrineysCompassion()
+  new MrBrineysCompassion(),
+  new TvReporter(),
 ];
