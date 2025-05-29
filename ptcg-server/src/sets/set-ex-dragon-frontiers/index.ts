2cad0ef61ae1d07be8dfa6133b310a6f848e0565
Benjamin Babcock <bencbabcock01@gmail.com>
2025-05-29 12:13:08 -0400

    Added Meganium DF

--- ptcg-server/src/sets/set-ex-dragon-frontiers/index.ts
+++ ptcg-server/src/sets/set-ex-dragon-frontiers/index.ts
@@ -1,12 +1,18 @@
 import { Card } from '../../game';
+import { Bayleef } from './bayleef';
+import { Chikorita } from './chikorita';
 import { Copycat } from './copycat';
 import { Mantine } from './mantine';
+import { Meganium } from './meganium';
 import { MrStonesProject } from './mr-stones-project';
 import { Rayquazaex } from './rayquaza-ex';
 
 export const setEXDragonFrontiers: Card[] = [
+  new Bayleef(),
+  new Chikorita(),
   new Copycat(),
   new Mantine(),
+  new Meganium(),
   new MrStonesProject(),
   new Rayquazaex()
 ];
