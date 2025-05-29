1f0c240aaeff0e3da7b90de38f785bacb7e2b38f
Benjamin Babcock <bencbabcock01@gmail.com>
2025-05-29 09:42:22 -0400

    Added Flygon line HP

--- ptcg-server/src/sets/set-ex-holon-phantoms/index.ts
+++ ptcg-server/src/sets/set-ex-holon-phantoms/index.ts
@@ -4,24 +4,30 @@ import { HolonAdventurer } from './holon-adventurer';
 import { DeltaRainbowEnergy } from './delta-rainbow-energy';
 import { Exeggcute } from './exeggcute';
 import { Exeggutor } from './exeggutor';
+import { Flygon } from './flygon';
 import { HolonsCastform } from './holons-castform';
 import { Meowth } from './meowth';
 import { Oddish } from './oddish';
 import { Pidgey } from './pidgey';
 import { Pikachu } from './pikachu';
 import { Raichu } from './raichu';
+import { Trapinch } from './trapinch';
+import { Vibrava } from './vibrava';
 
 export const setEXHolonPhantoms: Card[] = [
   new HolonAdventurer(),
   new DeltaRainbowEnergy(),
   new Exeggcute(),
   new Exeggutor(),
+  new Flygon(),
   new Meowth(),
   new Oddish(),
   new Pidgey(),
   new Pikachu(),
   new Raichu(),
   new HolonsCastform(),
+  new Trapinch(),
+  new Vibrava(),
 
   // Basic energies
   new GrassEnergy(),
