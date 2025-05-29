c47c761e14f3ef3c07a18676e9985a85d14296cf
Joe Myerscough <joey.myerscough@gmail.com>
2025-05-29 02:33:18 -0400

    Card Hashing

--- ptcg-server/src/game/cards/card-manager.ts
+++ ptcg-server/src/game/cards/card-manager.ts
@@ -1,22 +1,40 @@
 import { Card } from '../store/card/card';
 import { deepClone } from '../../utils/utils';
+import { CardsInfo } from '../../backend/interfaces/cards.interface';
 
 export class CardManager {
 
   private static instance: CardManager;
-
   private cards: Card[] = [];
+  private cardIndex: { [name: string]: number } = {};
 
   public static getInstance(): CardManager {
     if (!CardManager.instance) {
       CardManager.instance = new CardManager();
     }
-
     return CardManager.instance;
   }
 
-  public defineSet(cards: Card[]): void {
-    this.cards.push(...cards);
+  public defineSet(set: Card[]): void {
+    for (const card of set) {
+      let index = this.cardIndex[card.fullName];
+      if (index !== undefined && this.cards[index] !== card) {
+        throw new Error('Multiple cards with the same name: ' + card.fullName);
+      }
+      if (index === undefined) {
+        index = this.cards.length;
+        this.cardIndex[card.fullName] = index;
+        this.cards.push(card);
+      }
+    }
+  }
+
+  public loadCardsInfo(cardsInfo: CardsInfo) {
+    this.cardIndex = {};
+    this.cards = cardsInfo.cards;
+    for (let i = 0; i < this.cards.length; i++) {
+      this.cardIndex[this.cards[i].fullName] = i;
+    }
   }
 
   public defineCard(card: Card): void {
@@ -24,15 +42,14 @@ export class CardManager {
   }
 
   public getCardByName(name: string): Card | undefined {
-    let card = this.cards.find(c => c.fullName === name);
-    if (card !== undefined) {
-      card = deepClone(card);
+    const index = this.cardIndex[name];
+    if (index !== undefined) {
+      return deepClone(this.cards[index]);
     }
-    return card;
   }
 
   public isCardDefined(name: string): boolean {
-    return this.cards.find(c => c.fullName === name) !== undefined;
+    return this.cardIndex[name] !== undefined;
   }
 
   public getAllCards(): Card[] {
--- ptcg-server/src/sets/set-unified-minds/index.ts
+++ ptcg-server/src/sets/set-unified-minds/index.ts
@@ -42,10 +42,10 @@ import { Sneasel } from './sneasel';
 import { WeavileGX } from './weavile-gx';
 import { MegaSableyeTyranitarGX } from './mega-sableye-tyranitar-gx';
 import { PokemonResearchLab } from './pokemon-research-lab';
-import {UmbreonDarkraiGX} from './umbreon-and-darkrai-gx';
-import {SlowpokePsyduckGX} from './slowpoke-and-psyduck-gx';
-import {EspeonDeoxysGX} from './espeon-and-deoxys-gx';
-import {RowletAlolanExeggutorGX} from './rowlet-and-alolan-exeggutor-gx';
+import { UmbreonDarkraiGX } from './umbreon-and-darkrai-gx';
+import { SlowpokePsyduckGX } from './slowpoke-and-psyduck-gx';
+import { EspeonDeoxysGX } from './espeon-and-deoxys-gx';
+import { RowletAlolanExeggutorGX } from './rowlet-and-alolan-exeggutor-gx';
 import { Magnemite } from './magnemite';
 
 
@@ -61,6 +61,7 @@ export const setUnifiedMinds: Card[] = [
   new Drilbur(),
   new Eelektross(),
   new Elgyem(),
+  new EspeonDeoxysGX(),
   new FlyiniumZAirSlash(),
   new Froslass(),
   new GarchompGiratinaGX(),
@@ -74,6 +75,7 @@ export const setUnifiedMinds: Card[] = [
   new KeldeoGX(),
   new Magnemite(),
   new Lickilicky(),
+  new MegaSableyeTyranitarGX(),
   new MewtwoMewGX(),
   new Munchlax(),
   new Necrozma(),
@@ -84,18 +86,16 @@ export const setUnifiedMinds: Card[] = [
   new RecycleEnergy(),
   new ResetStamp(),
   new Riolu(),
+  new RowletAlolanExeggutorGX(),
+  new SlowpokePsyduckGX(),
   new SlumberingForest(),
+  new Sneasel(),
   new StadiumNav(),
   new TagSwitch(),
+  new UmbreonDarkraiGX(),
   new UTurnBoard(),
   new WeaknessGuardEnergy(),
   new Whimsicott(),
   new Wimpod(),
-  new Sneasel(),
   new WeavileGX(),
-  new MegaSableyeTyranitarGX(),
-  new UmbreonDarkraiGX(),
-  new SlowpokePsyduckGX(),
-  new EspeonDeoxysGX(),
-  new RowletAlolanExeggutorGX(),
 ];
