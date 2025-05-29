a74e64644fca87449870444adcaa838c8dde97be
Benjamin Babcock <bencbabcock01@gmail.com>
2025-05-29 12:52:32 -0400

    Added Meganium UF 106

--- ptcg-server/src/sets/set-ex-unseen-forces/index.ts
+++ ptcg-server/src/sets/set-ex-unseen-forces/index.ts
@@ -1,5 +1,8 @@
 import { Card } from '../../game';
+import { Bayleef } from './bayleef';
+import { Chikorita } from './chikorita';
 import { MarysRequest } from './marys-request';
+import { Meganiumex } from './meganium-ex';
 import { PokemonReversal } from './pokemon-reversal';
 import { Stantler } from './stantler';
 import { Slowpoke } from './slowpoke';
@@ -8,7 +11,10 @@ import { Typhlosionex } from './typhlosion-ex';
 import { Ursaring } from './ursaring';
 
 export const setEXUnseenForces: Card[] = [
+  new Bayleef(),
+  new Chikorita(),
   new MarysRequest(),
+  new Meganiumex(),
   new PokemonReversal(),
   new Stantler(),
   new Slowpoke(),
