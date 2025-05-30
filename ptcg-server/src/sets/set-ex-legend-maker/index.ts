51a13a0310f5f384e00f4afd95b91882f49c3f35
Benjamin Babcock <bencbabcock01@gmail.com>
2025-05-30 08:02:46 -0400

    Added Machamp line LM

--- ptcg-server/src/sets/set-ex-legend-maker/index.ts
+++ ptcg-server/src/sets/set-ex-legend-maker/index.ts
@@ -3,6 +3,9 @@ import { CursedStone } from './cursed-stone';
 import { GiantStump } from './giant-stump';
 import { Kecleon } from './kecleon';
 import { Lapras } from './lapras';
+import { Machamp } from './machamp';
+import { Machoke } from './machoke';
+import { Machop } from './machop';
 import { Mewex } from './mew-ex';
 import { Pikachu } from './pikachu';
 import { PowerTree } from './power-tree';
@@ -14,6 +17,9 @@ export const setEXLegendMaker: Card[] = [
   new GiantStump(),
   new Kecleon(),
   new Lapras(),
+  new Machamp(),
+  new Machoke(),
+  new Machop(),
   new Mewex(),
   new Pikachu(),
   new PowerTree(),
