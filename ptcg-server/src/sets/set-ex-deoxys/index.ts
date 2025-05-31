e31067f632c0dd0bdf9039ef0ca1f96ea766f345
Benjamin Babcock <bencbabcock01@gmail.com>
2025-05-31 07:37:49 -0400

    Added Lunatone DX

--- ptcg-server/src/sets/set-ex-deoxys/index.ts
+++ ptcg-server/src/sets/set-ex-deoxys/index.ts
@@ -6,6 +6,7 @@ import { Lombre } from './lombre';
 import { Lotad } from './lotad';
 import { Ludicolo } from './ludicolo';
 import { Ludicolo2 } from './ludicolo2';
+import { Lunatone } from './lunatone';
 import { Magcargo } from './magcargo';
 import { Minun } from './minun';
 import { Nosepass } from './nosepass';
@@ -21,6 +22,7 @@ export const setEXDeoxys: Card[] = [
   new Lotad(),
   new Ludicolo(),
   new Ludicolo2(),
+  new Lunatone(),
   new Magcargo(),
   new Minun(),
   new Nosepass(),
