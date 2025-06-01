cbafbf7639101791d0c7edddfcfa90ef6ad58adb
Benjamin Babcock <bencbabcock01@gmail.com>
2025-05-31 20:07:27 -0400

    Added Steelix ex and Onix UF

--- ptcg-server/src/sets/set-ex-unseen-forces/index.ts
+++ ptcg-server/src/sets/set-ex-unseen-forces/index.ts
@@ -1,17 +1,21 @@
 import { Card } from '../../game';
 import { MarysRequest } from './marys-request';
+import { Onix } from './onix';
 import { PokemonReversal } from './pokemon-reversal';
 import { Stantler } from './stantler';
 import { Slowpoke } from './slowpoke';
+import { Steelixex } from './steelix-ex';
 import { Teddiursa } from './teddiursa';
 import { Typhlosionex } from './typhlosion-ex';
 import { Ursaring } from './ursaring';
 
 export const setEXUnseenForces: Card[] = [
   new MarysRequest(),
+  new Onix(),
   new PokemonReversal(),
   new Stantler(),
   new Slowpoke(),
+  new Steelixex(),
   new Teddiursa(),
   new Typhlosionex(),
   new Ursaring(),
