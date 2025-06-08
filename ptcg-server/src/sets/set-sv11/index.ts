6493ae5dfb1b692fda3f772874585edfded19976
Joe Myerscough <joey.myerscough@gmail.com>
2025-06-08 15:04:20 -0400

    SV11 Update

--- ptcg-server/src/sets/set-sv11/index.ts
+++ ptcg-server/src/sets/set-sv11/index.ts
@@ -4,8 +4,11 @@ import { Amoongus } from './amoongus';
 import { AntiquePlumeFossil } from './antique-plume-fossil';
 import { Archen } from './archen';
 import { Archeops } from './archeops';
+import { Axew } from './axew';
+import { BraveBangle } from './brave-bangle';
 import { Carracosta } from './carracosta';
 import { Cobalion } from './cobalion';
+import { Cottonee } from './cottonee';
 import { Dewott } from './dewott';
 import { Drillbur } from './drillbur';
 import { Duosion } from './duosion';
@@ -16,15 +19,20 @@ import { EnergyCoin } from './energy-coin';
 import { Excadrillex } from './excadrill-ex';
 import { Fennel } from './fennel';
 import { Foongus } from './foongus';
+import { Fraxure } from './fraxure';
+import { Frillish } from './frillish';
 import { Genesectex } from './genesect-ex';
+import { Haxorus } from './haxorus';
 import { Hilda } from './hilda';
 import { IgnitionEnergy } from './ignition-energy';
+import { Jellicentex } from './jellicent-ex';
 import { Keldeoex } from './keldeo-ex';
 import { Kyuremex } from './kyurem-ex';
 import { Larvesta } from './larvesta';
 import { Meloettaex } from './meloetta-ex';
+import { NsPlot } from './ns-plot';
 import { Oshawott } from './oshawott';
-import { AirBalloonSV11, CherenSV11, EnergyRetrievalSV11, PrismEnergySV11, ProfessorsResearchSV11 } from './other-prints';
+import { AirBalloonSV11, CarracostaAR, AlomomolaAR, CherenSV11, EnergyRetrievalSV11, LarvestaAR, PrismEnergySV11, ProfessorsResearchSV11, SeismitoadAR, TirtougaAR, ServineAR, SnivyAR, TympoleAR, VictiniAR, VolcaronaAR, HaxorusAR, FraxureAR, AxewAR, CobalionAR, DrillburAR, EelektrikAR, CottoneeAR, EmolgaAR, PalpitoadAR, TynamoAR, ExcadrillexFA, FennelFA, GenesectexFA, KyuremexFA, MeloettaexFA, NsPlotFA, SerperiorexFA, ZekromexFA, ExcadrillexSIR, GenesectexSIR, KyuremexSIR, MeloettaexSIR, NsPlotSIR, SerperiorexSIR, ZekromexSIR, ZekromexBR, JoltikAR, GalvantulaAR, DewottAR, EmboarAR, FrillishAR, OshawottAR, PigniteAR, SamurottAR, TepigAR, VirizionAR, ArchenAR, ArcheopsAR, DeinoAR, TerrakionAR, ZoroarkAR, ZoruaAR, ZweilousAR, ReshiramexFA, KeldeoexFA, WhimsicottexFA, HydreigonexFA, JellicentexFA, BouffalantexFA, HildaFA, HildaSIR, BouffalantexSIR, HydreigonexSIR, JellicentexSIR, KeldeoexSIR, ReshiramexSIR, WhimsicottexSIR, ReshiramexWR } from './other-prints';
 import { Palpitoad } from './palpitoad';
 import { Pignite } from './pignite';
 import { Reshiramex } from './reshiram-ex';
@@ -43,6 +51,7 @@ import { Tynamo } from './tynamo';
 import { Victini } from './victini';
 import { Virizion } from './virizion';
 import { Volcarona } from './volcarona';
+import { Whimsicottex } from './whimsicott-ex';
 import { Zekromex } from './zekrom-ex';
 import { Zoroark } from './zoroark';
 import { Zorua } from './zorua';
@@ -105,4 +114,98 @@ export const setSV11: Card[] = [
   new IgnitionEnergy(),
   new PrismEnergySV11(),
 
+  new Axew(),
+  new Fraxure(),
+  new Haxorus(),
+  new Cottonee(),
+  new Whimsicottex(),
+  new NsPlot(),
+  new BraveBangle(),
+
+  new Frillish(),
+  new Jellicentex(),
+
+  //SV11B ARs
+  new SnivyAR(),
+  new ServineAR(),
+  new VictiniAR(),
+  new LarvestaAR(),
+  new VolcaronaAR(),
+  new TympoleAR(),
+  new PalpitoadAR(),
+  new SeismitoadAR(),
+  new TirtougaAR(),
+  new CarracostaAR(),
+  new AlomomolaAR(),
+  new EmolgaAR(),
+  new TynamoAR(),
+  new EelektrikAR(),
+  new DrillburAR(),
+  new CobalionAR(),
+  new AxewAR(),
+  new FraxureAR(),
+  new HaxorusAR(),
+
+  //SV11B FA
+  new SerperiorexFA(),
+  new KyuremexFA(),
+  new ZekromexFA(),
+  new MeloettaexFA(),
+  new ExcadrillexFA(),
+  new GenesectexFA(),
+  new NsPlotFA(),
+  new FennelFA(),
+
+  //SV11B SIR
+  new SerperiorexSIR(),
+  new KyuremexSIR(),
+  new ZekromexSIR(),
+  new MeloettaexSIR(),
+  new ExcadrillexSIR(),
+  new GenesectexSIR(),
+  new NsPlotSIR(),
+
+  //SV11B BR
+  new ZekromexBR(),
+
+  //SV11W ARs
+  new CottoneeAR(),
+  new VirizionAR(),
+  new TepigAR(),
+  new PigniteAR(),
+  new EmboarAR(),
+  new OshawottAR(),
+  new DewottAR(),
+  new SamurottAR(),
+  new JoltikAR(),
+  new GalvantulaAR(),
+  new FrillishAR(),
+  new ArchenAR(),
+  new ArcheopsAR(),
+  new TerrakionAR(),
+  new ZoruaAR(),
+  new ZoroarkAR(),
+  new DeinoAR(),
+  new ZweilousAR(),
+
+  //SV11W FA
+  new WhimsicottexFA(),
+  new ReshiramexFA(),
+  new KeldeoexFA(),
+  new JellicentexFA(),
+  new HydreigonexFA(),
+  new BouffalantexFA(),
+  new HildaFA(),
+
+  //SV11W SIR
+  new WhimsicottexSIR(),
+  new ReshiramexSIR(),
+  new KeldeoexSIR(),
+  new JellicentexSIR(),
+  new HydreigonexSIR(),
+  new BouffalantexSIR(),
+  new HildaSIR(),
+
+  //SV11W WR
+  new ReshiramexWR(),
 ];
