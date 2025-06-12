d8191d4cf33012b9b0e9384a1f01237aea7e1e74
Joe Myerscough <joey.myerscough@gmail.com>
2025-06-11 21:59:08 -0400

    Choice Band Fix

--- ptcg-server/src/sets/set-guardians-rising/choice-band.ts
+++ ptcg-server/src/sets/set-guardians-rising/choice-band.ts
@@ -4,28 +4,20 @@ import { StoreLike } from '../../game/store/store-like';
 import { State } from '../../game/store/state/state';
 import { Effect } from '../../game/store/effects/effect';
 import { StateUtils } from '../../game/store/state-utils';
-import { DealDamageEffect } from '../../game/store/effects/attack-effects';
-import {ToolEffect} from '../../game/store/effects/play-card-effects';
+import { PutDamageEffect } from '../../game/store/effects/attack-effects';
+import { ToolEffect } from '../../game/store/effects/play-card-effects';
 
 export class ChoiceBand extends TrainerCard {
-
   public trainerType: TrainerType = TrainerType.TOOL;
-
   public set: string = 'GRI';
-
   public setNumber = '121';
-
   public cardImage = 'assets/cardback.png';
-
   public name: string = 'Choice Band';
-
   public fullName: string = 'Choice Band GRI';
-
-  public text: string =
-    'The attacks of the Pokémon this card is attached to do 30 more damage to your opponent\'s Active Pokémon-GX or Active Pokémon-EX (before applying Weakness and Resistance).';
+  public text: string = 'The attacks of the Pokémon this card is attached to do 30 more damage to your opponent\'s Active Pokémon-GX or Active Pokémon-EX (before applying Weakness and Resistance).';
 
   public reduceEffect(store: StoreLike, state: State, effect: Effect): State {
-    if (effect instanceof DealDamageEffect && effect.source.cards.includes(this)) {
+    if (effect instanceof PutDamageEffect && effect.source.cards.includes(this)) {
       const opponent = StateUtils.getOpponent(state, effect.player);
       const defending = opponent.active.getPokemonCard();
 
@@ -37,9 +29,7 @@ export class ChoiceBand extends TrainerCard {
         return state;
       }
 
-      const attack = effect.attack;
-      if (attack && attack.damage > 0 && effect.target === opponent.active && defending
-        && (defending.tags.includes(CardTag.POKEMON_GX) || defending.tags.includes(CardTag.POKEMON_EX))) {
+      if (effect.damage > 0 && effect.target === opponent.active && defending && defending.tags.includes(CardTag.POKEMON_GX)) {
         effect.damage += 30;
       }
     }
