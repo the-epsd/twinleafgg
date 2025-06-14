1b5fa4a354ceca4940a38e14957715bbf78a1e0f
Joe Myerscough <joey.myerscough@gmail.com>
2025-06-14 18:24:02 -0400

    Card Bug Fixes

--- ptcg-server/src/sets/set-emerging-powers/index.ts
+++ ptcg-server/src/sets/set-emerging-powers/index.ts
@@ -4,6 +4,7 @@ import { CrushingHammerEPO, RecycleEPO } from './other-prints';
 import { Cheren } from './cheren';
 import { MaxPotion } from './max-potion';
 import { Tornadus } from './tornadus';
+import { PokemonCatcher } from './pokemon-catcher';
 
 export const setEmergingPowers: Card[] = [
   new Bianca(),
@@ -11,5 +12,6 @@ export const setEmergingPowers: Card[] = [
   new CrushingHammerEPO(),
   new MaxPotion(),
   new RecycleEPO(),
+  new PokemonCatcher(),
   new Tornadus(),
 ];
