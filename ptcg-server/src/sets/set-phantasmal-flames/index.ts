c3938f307aa277de6174902cb08d421e9016e30a
Joe Myerscough <joey.myerscough@gmail.com>
2025-08-22 00:25:52 -0400

    Mega Charizard X, Oricorio

--- ptcg-server/src/sets/set-phantasmal-flames/index.ts
+++ ptcg-server/src/sets/set-phantasmal-flames/index.ts
@@ -1,11 +1,15 @@
 import { Card } from "../../game";
 import { Gastly } from "./gastly";
 import { Haunter } from "./haunter";
+import { MegaCharizardXex } from "./mega-charizard-x-ex";
 import { MegaGengarex } from "./mega-gengar-ex";
+import { Oricorioex } from "./oricorio-ex";
 
 
 export const setPhantasmalFlames: Card[] = [
   new Gastly(),
   new Haunter(),
   new MegaGengarex(),
+  new Oricorioex(),
+  new MegaCharizardXex(),
 ];
