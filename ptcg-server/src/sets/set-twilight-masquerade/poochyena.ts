e5b51064124c9a4f5d7ce1bcb897eccc90e4e7f9
Joe Myerscough <joey.myerscough@gmail.com>
2025-08-28 18:33:45 -0400

    Poochyena TWM

--- ptcg-server/src/sets/set-twilight-masquerade/poochyena.ts
+++ ptcg-server/src/sets/set-twilight-masquerade/poochyena.ts
@@ -1,33 +1,30 @@
 import { PokemonCard } from '../../game/store/card/pokemon-card';
 import { Stage, CardType } from '../../game/store/card/card-types';
 import { StoreLike, State, CoinFlipPrompt, GameMessage } from '../../game';
-import { AttackEffect } from '../../game/store/effects/game-effects';
 import { Effect } from '../../game/store/effects/effect';
+import { WAS_ATTACK_USED } from '../../game/store/prefabs/prefabs';
 
 export class Poochyena extends PokemonCard {
-
   public stage: Stage = Stage.BASIC;
-  public cardType: CardType = CardType.DARK;
+  public cardType: CardType = D;
   public hp: number = 70;
-  public weakness = [{ type: CardType.GRASS }];
+  public weakness = [{ type: G }];
   public resistance = [];
-  public retreat = [CardType.COLORLESS];
+  public retreat = [C];
 
-  public attacks = [
-    {
-      name: 'Continuous Steps',
-      cost: [CardType.DARK],
-      damage: 10,
-      damageCalculation: 'x',
-      text: 'Flip a coin until you get tails. This attack does 10 damage for each heads.'
-    },
-    {
-      name: 'Darkness Fang',
-      cost: [CardType.DARK, CardType.COLORLESS],
-      damage: 20,
-      text: ''
-    },
-  ];
+  public attacks = [{
+    name: 'Continuous Steps',
+    cost: [D],
+    damage: 10,
+    damageCalculation: 'x',
+    text: 'Flip a coin until you get tails. This attack does 10 damage for each heads.'
+  },
+  {
+    name: 'Darkness Fang',
+    cost: [D, C],
+    damage: 20,
+    text: ''
+  }];
 
   public regulationMark = 'H';
   public set: string = 'TWM';
@@ -38,21 +35,26 @@ export class Poochyena extends PokemonCard {
 
   public reduceEffect(store: StoreLike, state: State, effect: Effect): State {
 
-    if (effect instanceof AttackEffect && effect.attack === this.attacks[0]) {
+    if (WAS_ATTACK_USED(effect, 0, this)) {
       const player = effect.player;
+      let headsCount = 0;
 
-      const flipCoin = (heads: number = 0): State => {
+      const flipUntilTails = (): State => {
         return store.prompt(state, [
           new CoinFlipPrompt(player.id, GameMessage.COIN_FLIP)
         ], result => {
-          if (result === true) {
-            return flipCoin(heads + 1);
+          if (result) {
+            // Heads - increment count and flip again
+            headsCount++;
+            return flipUntilTails();
+          } else {
+            // Tails - calculate final damage
+            effect.damage = 10 * headsCount;
+            return state;
           }
-          effect.damage = 10 * heads;
-          return state;
         });
       };
-      return flipCoin();
+      return flipUntilTails();
     }
     return state;
   }
