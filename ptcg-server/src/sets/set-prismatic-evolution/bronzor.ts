a95072ccf6444171ff2d93fbdfbe18b241633342
Joe Myerscough <joey.myerscough@gmail.com>
2026-02-04 01:17:48 -0500

    bronzong tef

--- ptcg-server/src/sets/set-prismatic-evolution/bronzor.ts
+++ ptcg-server/src/sets/set-prismatic-evolution/bronzor.ts
@@ -1,24 +1,22 @@
+import { Stage, CardType } from '../../game/store/card/card-types';
 import { PokemonCard } from '../../game/store/card/pokemon-card';
-import { CardType, Stage } from '../../game/store/card/card-types';
 
 export class Bronzor extends PokemonCard {
   public stage: Stage = Stage.BASIC;
-  public cardType: CardType = M;
   public hp: number = 70;
+  public cardType: CardType = M;
   public weakness = [{ type: R }];
   public resistance = [{ type: G, value: -30 }];
-  public retreat = [ C ];
+  public retreat = [C];
 
-  public attacks = [
-    {
-      name: 'Hammer In',
-      cost: [ M, C ],
-      damage: 20,
-      text: '',
-    }
-  ];
+  public attacks = [{
+    name: 'Hammer In',
+    cost: [M, C],
+    damage: 20,
+    text: ''
+  }];
 
-  public regulationMark = 'H';
+  public regulationMark: string = 'H';
   public set: string = 'PRE';
   public cardImage: string = 'assets/cardback.png';
   public setNumber: string = '66';
