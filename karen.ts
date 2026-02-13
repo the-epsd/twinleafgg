450c5867d3404ed28adebd9bfac475b24570bd60
Hella_Github <161643948+Lavenza-Hellard@users.noreply.github.com>
2026-02-13 22:54:46 +0200

    Fix for Karen
    Wasn't shuffling pokemon at all

---
+++ karen.ts
@@ -0,0 +1,33 @@
+import { Effect } from '../../game/store/effects/effect';
+import { TrainerEffect } from '../../game/store/effects/play-card-effects';
+import { State } from '../../game/store/state/state';
+import { StoreLike } from '../../game/store/store-like';
+import { TrainerCard } from '../../game/store/card/trainer-card';
+import { TrainerType, SuperType } from '../../game/store/card/card-types';
+import { StateUtils } from '../../game/store/state-utils';
+import { SHUFFLE_DECK } from '../../game/store/prefabs/prefabs';
+
+export class Karen extends TrainerCard {
+
+  public trainerType: TrainerType = TrainerType.SUPPORTER;
+  public set: string = 'XYP';
+  public name: string = 'Karen';
+  public fullName: string = 'Karen XYP';
+  public cardImage: string = 'assets/cardback.png';
+  public setNumber: string = '177';
+
+  public text: string = 'Each player shuffles all Pokémon in his or her discard pile into his or her deck.';
+
+  public reduceEffect(store: StoreLike, state: State, effect: Effect): State {
+    if (effect instanceof TrainerEffect && effect.trainerCard === this) {
+      for (const p of [effect.player, StateUtils.getOpponent(state, effect.player)]) {
+        const discardedPokemon = p.discard.cards.filter(c => c.superType === SuperType.POKEMON);
+       if (discardedPokemon.length > 0) {
+          p.discard.moveCardsTo(discardedPokemon, p.deck);
+          SHUFFLE_DECK(store, state, p);
+        }
+      }
+    }
+    return state;
+  }
+}
