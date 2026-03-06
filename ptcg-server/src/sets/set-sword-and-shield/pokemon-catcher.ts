de33301bdbf692100ee5d3af702f634d5d47a650
Joe Myerscough <joey.myerscough@gmail.com>
2026-03-06 15:21:24 -0500

    Pokemon Catcher Fix

--- ptcg-server/src/sets/set-sword-and-shield/pokemon-catcher.ts
+++ ptcg-server/src/sets/set-sword-and-shield/pokemon-catcher.ts
@@ -4,8 +4,8 @@ import { StoreLike } from '../../game/store/store-like';
 import { State } from '../../game/store/state/state';
 import { Effect } from '../../game/store/effects/effect';
 import { ChoosePokemonPrompt } from '../../game/store/prompts/choose-pokemon-prompt';
-import { TrainerEffect } from '../../game/store/effects/play-card-effects';
-import { PlayerType, SlotType, CoinFlipPrompt, StateUtils, GameError, GameMessage } from '../../game';
+import { TrainerEffect, CoinFlipEffect } from '../../game/store/effects/play-card-effects';
+import { PlayerType, SlotType, StateUtils, GameError, GameMessage } from '../../game';
 
 function* playCard(next: Function, store: StoreLike, state: State, effect: TrainerEffect): IterableIterator<State> {
   const player = effect.player;
@@ -17,13 +17,13 @@ function* playCard(next: Function, store: StoreLike, state: State, effect: Train
   }
 
   let coinResult: boolean = false;
-  yield store.prompt(state, new CoinFlipPrompt(player.id, GameMessage.COIN_FLIP), result => {
+  const coinFlipEffect = new CoinFlipEffect(player, (result: boolean) => {
     coinResult = result;
     next();
   });
+  yield store.reduceEffect(state, coinFlipEffect);
 
   if (coinResult === false) {
-
     return state;
   }
 
@@ -41,15 +41,11 @@ function* playCard(next: Function, store: StoreLike, state: State, effect: Train
 }
 
 export class PokemonCatcher extends TrainerCard {
-
   public trainerType: TrainerType = TrainerType.ITEM;
-
   public regulationMark = 'G';
-
-  public set: string = 'BW';
-
+  public set: string = 'SSH';
+  public setNumber: string = '175';
   public name: string = 'Pokémon Catcher';
-
   public fullName: string = 'Pokemon Catcher SSH';
 
   public text: string =
