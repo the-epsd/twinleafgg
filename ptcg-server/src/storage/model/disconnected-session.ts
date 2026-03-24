59e365debb0ea3f74df9c247bd6921d5f5d07b8d
Benjamin Babcock <bencbabcock01@gmail.com>
2026-03-24 10:50:49 -0400

    disconnected-session error fix

--- ptcg-server/src/storage/model/disconnected-session.ts
+++ ptcg-server/src/storage/model/disconnected-session.ts
@@ -14,7 +14,7 @@ export class DisconnectedSession extends BaseEntity {
   @Column()
   public gameId!: number;
 
-  @Column({ type: 'mediumtext' })
+  @Column({ type: 'text' })
   public gameState!: string; // Serialized game state
 
   @Column({ type: 'bigint', transformer: [bigint] })
