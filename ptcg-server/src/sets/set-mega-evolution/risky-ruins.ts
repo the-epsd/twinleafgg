33598e41216b89dc46c0334b55afc5ebd017c9ff
Eskaven <skelothan@gmail.com>
2026-05-14 21:50:48 -0700

    fix(cards): minor text changes

--- ptcg-server/src/sets/set-mega-evolution/risky-ruins.ts
+++ ptcg-server/src/sets/set-mega-evolution/risky-ruins.ts
@@ -2,7 +2,10 @@ import { State, StateUtils, StoreLike } from '../../game';
 import { CardType, Stage, TrainerType } from '../../game/store/card/card-types';
 import { TrainerCard } from '../../game/store/card/trainer-card';
 import { Effect } from '../../game/store/effects/effect';
-import { PlayPokemonEffect, PlayPokemonFromDeckEffect } from '../../game/store/effects/play-card-effects';
+import {
+  PlayPokemonEffect,
+  PlayPokemonFromDeckEffect,
+} from '../../game/store/effects/play-card-effects';
 
 export class DangerousRuins extends TrainerCard {
   public regulationMark = 'I';
@@ -12,12 +15,19 @@ export class DangerousRuins extends TrainerCard {
   public set = 'MEG';
   public name = 'Risky Ruins';
   public fullName = 'Risky Ruins MEG';
-  public text = 'Whenever either player puts a non-[D] Basic Pokémon onto their Bench, put 2 damage counters on that Pokémon.';
+  public text =
+    'Whenever any player puts a Basic non-[D] Pokémon onto their Bench, put 2 damage counters on that Pokémon.';
 
   reduceEffect(store: StoreLike, state: State, effect: Effect): State {
-
-    if ((effect instanceof PlayPokemonEffect || effect instanceof PlayPokemonFromDeckEffect) && StateUtils.getStadiumCard(state) === this) {
-      if (effect.target.cards.length > 0 || effect.pokemonCard.cardType === CardType.DARK) {
+    if (
+      (effect instanceof PlayPokemonEffect ||
+        effect instanceof PlayPokemonFromDeckEffect) &&
+      StateUtils.getStadiumCard(state) === this
+    ) {
+      if (
+        effect.target.cards.length > 0 ||
+        effect.pokemonCard.cardType === CardType.DARK
+      ) {
         return state;
       }
 
@@ -29,5 +39,4 @@ export class DangerousRuins extends TrainerCard {
 
     return state;
   }
-
 }
--- ptcg-server/src/sets/set-stellar-crown/terapagos-ex.ts
+++ ptcg-server/src/sets/set-stellar-crown/terapagos-ex.ts
@@ -1,68 +1,65 @@
 import { PokemonCard } from '../../game/store/card/pokemon-card';
 import { Stage, CardType, CardTag } from '../../game/store/card/card-types';
-import { StoreLike, State, StateUtils, GameError, GameMessage } from '../../game';
+import {
+  StoreLike,
+  State,
+  StateUtils,
+  GameError,
+  GameMessage,
+} from '../../game';
 import { Effect } from '../../game/store/effects/effect';
 
 import { PutDamageEffect } from '../../game/store/effects/attack-effects';
 import { WAS_ATTACK_USED } from '../../game/store/prefabs/prefabs';
 
 export class Terapagosex extends PokemonCard {
-
   public regulationMark = 'H';
-
   public tags = [CardTag.POKEMON_ex, CardTag.POKEMON_TERA];
-
   public stage: Stage = Stage.BASIC;
-
-  public cardType: CardType = CardType.COLORLESS;
-
+  public cardType: CardType = C;
   public hp: number = 230;
-
-  public weakness = [{ type: CardType.FIGHTING }];
-
-  public retreat = [CardType.COLORLESS, CardType.COLORLESS];
+  public weakness = [{ type: F }];
+  public retreat = [C, C];
 
   public attacks = [
     {
       name: 'Unified Beatdown',
-      cost: [CardType.COLORLESS, CardType.COLORLESS],
+      cost: [C, C],
       damage: 30,
       damageCalculation: 'x',
-      text: 'If you go second, you can\'t use this attack during your first turn.This attack does 30 damage for each of your Benched Pokémon.'
+      text: "If you go second, you can't use this attack during your first turn. This attack does 30 damage for each of your Benched Pokémon.",
     },
     {
       name: 'Crown Opal',
-      cost: [CardType.GRASS, CardType.WATER, CardType.LIGHTNING],
+      cost: [G, W, L],
       damage: 180,
-      damageCalculation: '+',
-      text: 'During your opponent\'s next turn, prevent all damage done to this Pokémon by attacks from Basic non-[C] Pokémon.'
+      text: "During your opponent's next turn, prevent all damage done to this Pokémon by attacks from Basic non-[C] Pokémon.",
     },
   ];
 
   public set: string = 'SCR';
-
   public cardImage: string = 'assets/cardback.png';
-
   public setNumber: string = '128';
-
   public name: string = 'Terapagos ex';
-
   public fullName: string = 'Terapagos ex SCR';
 
-  public readonly PREVENT_DAMAGE_DURING_OPPONENTS_NEXT_TURN_MARKER = 'PREVENT_DAMAGE_DURING_OPPONENTS_NEXT_TURN_MARKER';
-  public readonly CLEAR_PREVENT_DAMAGE_DURING_OPPONENTS_NEXT_TURN_MARKER = 'CLEAR_PREVENT_DAMAGE_DURING_OPPONENTS_NEXT_TURN_MARKER';
+  public readonly PREVENT_DAMAGE_DURING_OPPONENTS_NEXT_TURN_MARKER =
+    'PREVENT_DAMAGE_DURING_OPPONENTS_NEXT_TURN_MARKER';
+  public readonly CLEAR_PREVENT_DAMAGE_DURING_OPPONENTS_NEXT_TURN_MARKER =
+    'CLEAR_PREVENT_DAMAGE_DURING_OPPONENTS_NEXT_TURN_MARKER';
 
   public reduceEffect(store: StoreLike, state: State, effect: Effect): State {
-
     if (WAS_ATTACK_USED(effect, 0, this)) {
-
       if (state.turn <= 2) {
         throw new GameError(GameMessage.CANNOT_USE_ATTACK);
       }
 
       const player = effect.player;
 
-      const playerBench = player.bench.reduce((left, b) => left + (b.cards.length ? 1 : 0), 0);
+      const playerBench = player.bench.reduce(
+        (left, b) => left + (b.cards.length ? 1 : 0),
+        0,
+      );
 
       const totalBenched = playerBench;
 
@@ -72,25 +69,47 @@ export class Terapagosex extends PokemonCard {
     if (WAS_ATTACK_USED(effect, 1, this)) {
       const player = effect.player;
       const opponent = StateUtils.getOpponent(state, player);
-      player.active.marker.addMarker(this.PREVENT_DAMAGE_DURING_OPPONENTS_NEXT_TURN_MARKER, this);
-      opponent.marker.addMarker(this.CLEAR_PREVENT_DAMAGE_DURING_OPPONENTS_NEXT_TURN_MARKER, this);
+      player.active.marker.addMarker(
+        this.PREVENT_DAMAGE_DURING_OPPONENTS_NEXT_TURN_MARKER,
+        this,
+      );
+      opponent.marker.addMarker(
+        this.CLEAR_PREVENT_DAMAGE_DURING_OPPONENTS_NEXT_TURN_MARKER,
+        this,
+      );
     }
 
-    if (effect instanceof PutDamageEffect && effect.target.marker.hasMarker(this.PREVENT_DAMAGE_DURING_OPPONENTS_NEXT_TURN_MARKER)) {
+    if (
+      effect instanceof PutDamageEffect &&
+      effect.target.marker.hasMarker(
+        this.PREVENT_DAMAGE_DURING_OPPONENTS_NEXT_TURN_MARKER,
+      )
+    ) {
       const sourceCard = effect.source.getPokemonCard();
-      if (sourceCard && sourceCard.stage === Stage.BASIC && sourceCard.cardType !== CardType.COLORLESS) {
+      if (
+        sourceCard &&
+        sourceCard.stage === Stage.BASIC &&
+        sourceCard.cardType !== CardType.COLORLESS
+      ) {
         effect.preventDefault = true;
         return state;
       }
       return state;
     }
 
-    if (effect instanceof PutDamageEffect && effect.target.cards.includes(this) && effect.target.getPokemonCard() === this) {
+    if (
+      effect instanceof PutDamageEffect &&
+      effect.target.cards.includes(this) &&
+      effect.target.getPokemonCard() === this
+    ) {
       const player = effect.player;
       const opponent = StateUtils.getOpponent(state, player);
 
       // Target is not Active
-      if (effect.target === player.active || effect.target === opponent.active) {
+      if (
+        effect.target === player.active ||
+        effect.target === opponent.active
+      ) {
         return state;
       }
 
