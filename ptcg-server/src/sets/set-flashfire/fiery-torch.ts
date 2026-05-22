86298111634195a9d5a3a0e1a13edd556c1523e5
Eskaven <skelothan@gmail.com>
2026-05-22 05:08:26 -0700

    fix(cards): Fiery Torch draws correct number

--- ptcg-server/src/sets/set-flashfire/fiery-torch.ts
+++ ptcg-server/src/sets/set-flashfire/fiery-torch.ts
@@ -6,17 +6,28 @@ import { EnergyType, SuperType, TrainerType } from '../../game/store/card/card-t
 import { TrainerCard } from '../../game/store/card/trainer-card';
 import { Effect } from '../../game/store/effects/effect';
 import { TrainerEffect } from '../../game/store/effects/play-card-effects';
+import { DRAW_CARDS } from '../../game/store/prefabs/prefabs';
 import { ChooseCardsPrompt } from '../../game/store/prompts/choose-cards-prompt';
 import { State } from '../../game/store/state/state';
 import { StoreLike } from '../../game/store/store-like';
 
-function* playCard(next: Function, store: StoreLike, state: State,
-  self: FieryTorch, effect: TrainerEffect): IterableIterator<State> {
-
+function* playCard(
+  next: Function,
+  store: StoreLike,
+  state: State,
+  self: FieryTorch,
+  effect: TrainerEffect,
+): IterableIterator<State> {
   const player = effect.player;
   let cards: Card[] = [];
 
-  cards = player.hand.cards.filter(c => c !== self && c instanceof EnergyCard && c.energyType === EnergyType.BASIC && c.name === 'Fire Energy');
+  cards = player.hand.cards.filter(
+    (c) =>
+      c !== self &&
+      c instanceof EnergyCard &&
+      c.energyType === EnergyType.BASIC &&
+      c.name === 'Fire Energy',
+  );
 
   if (cards.length < 1) {
     throw new GameError(GameMessage.CANNOT_PLAY_THIS_CARD);
@@ -30,16 +41,20 @@ function* playCard(next: Function, store: StoreLike, state: State,
   effect.preventDefault = true;
   player.hand.moveCardTo(effect.trainerCard, player.supporter);
 
-  yield store.prompt(state, new ChooseCardsPrompt(
-    player,
-    GameMessage.CHOOSE_CARD_TO_DISCARD,
-    player.hand,
-    { superType: SuperType.ENERGY, energyType: EnergyType.BASIC, name: 'Fire Energy' },
-    { min: 1, max: 1, allowCancel: false }
-  ), selected => {
-    cards = selected || [];
-    next();
-  });
+  yield store.prompt(
+    state,
+    new ChooseCardsPrompt(
+      player,
+      GameMessage.CHOOSE_CARD_TO_DISCARD,
+      player.hand,
+      { superType: SuperType.ENERGY, energyType: EnergyType.BASIC, name: 'Fire Energy' },
+      { min: 1, max: 1, allowCancel: false },
+    ),
+    (selected) => {
+      cards = selected || [];
+      next();
+    },
+  );
 
   // Operation canceled by the user
   if (cards.length === 0) {
@@ -47,32 +62,23 @@ function* playCard(next: Function, store: StoreLike, state: State,
   }
 
   player.hand.moveCardsTo(cards, player.discard);
-  player.deck.moveTo(player.hand, 3);
-
+  DRAW_CARDS(player, 2);
 
   return state;
 }
 
 export class FieryTorch extends TrainerCard {
-
   public trainerType: TrainerType = TrainerType.ITEM;
-
   public set: string = 'FLF';
-
   public cardImage: string = 'assets/cardback.png';
-
   public setNumber: string = '89';
-
   public name: string = 'Fiery Torch';
-
   public fullName: string = 'Fiery Torch FLF';
 
   public text: string =
-    'Discard a [R] Energy card from your hand. (If you can\'t discard a [R] Energy card, you can\'t play this card.) Draw 2 cards.';
-
+    "Discard a [R] Energy card from your hand. (If you can't discard a [R] Energy card, you can't play this card.) Draw 2 cards.";
 
   public reduceEffect(store: StoreLike, state: State, effect: Effect): State {
-
     if (effect instanceof TrainerEffect && effect.trainerCard === this) {
       const generator = playCard(() => generator.next(), store, state, this, effect);
       return generator.next().value;
@@ -80,5 +86,4 @@ export class FieryTorch extends TrainerCard {
 
     return state;
   }
-
 }
