c1f7775ef7d60d5c58d3cc359b3acafcfafe986e
Eskaven <skelothan@gmail.com>
2026-05-22 16:16:54 -0700

    fix(cards): various CRI bug fixes
    - Goodra CRI's attack cost
    - Mareep CRI's damage
    - Ho-Oh CRI only needs to discard [R] Energy
    - Mega Gallade ex's Gale Slash does more damage when it has *no* damage
    - Metagross CRI's Bounce Back attack cost
    - Ninetales CRI's Nine-Tailed Transfer moves counters from its controller's Bench
    - Phanpy CRI's HP
    - Unmerged Watchog CRI's attacks

--- ptcg-server/src/sets/set-chaos-rising/mareep.ts
+++ ptcg-server/src/sets/set-chaos-rising/mareep.ts
@@ -1,7 +1,11 @@
 import { CardType, Stage } from '../../game/store/card/card-types';
 import { Effect } from '../../game/store/effects/effect';
 import { PokemonCard, StoreLike, State } from '../../game';
-import { ADD_PARALYZED_TO_PLAYER_ACTIVE, AFTER_ATTACK, COIN_FLIP_PROMPT } from '../../game/store/prefabs/prefabs';
+import {
+  ADD_PARALYZED_TO_PLAYER_ACTIVE,
+  AFTER_ATTACK,
+  COIN_FLIP_PROMPT,
+} from '../../game/store/prefabs/prefabs';
 
 export class Mareep extends PokemonCard {
   public stage: Stage = Stage.BASIC;
@@ -10,15 +14,17 @@ export class Mareep extends PokemonCard {
   public weakness = [{ type: F }];
   public retreat = [C];
 
-  public attacks = [{
-    name: 'Thunder Wave',
-    cost: [L, C],
-    damage: 30,
-    text: 'Flip a coin. If heads, your opponent\'s Active Pokemon is now Paralyzed.'
-  }];
+  public attacks = [
+    {
+      name: 'Thunder Wave',
+      cost: [L, C],
+      damage: 20,
+      text: "Flip a coin. If heads, your opponent's Active Pokémon is now Paralyzed.",
+    },
+  ];
 
   public regulationMark = 'J';
-  public set: string = 'M4';
+  public set: string = 'CRI';
   public cardImage: string = 'assets/cardback.png';
   public setNumber: string = '27';
   public name: string = 'Mareep';
@@ -26,7 +32,7 @@ export class Mareep extends PokemonCard {
 
   public reduceEffect(store: StoreLike, state: State, effect: Effect): State {
     if (AFTER_ATTACK(effect, 0, this)) {
-      COIN_FLIP_PROMPT(store, state, effect.player, result => {
+      COIN_FLIP_PROMPT(store, state, effect.player, (result) => {
         if (result) {
           ADD_PARALYZED_TO_PLAYER_ACTIVE(store, state, effect.opponent, this);
         }
