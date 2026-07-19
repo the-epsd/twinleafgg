eb8a49422ee6b717b3c8f545a3e7d58c56c69bdd
Eskaven <skelothan@gmail.com>
2026-07-19 11:22:20 -0700

    feat(cards): PBL secret rares

---
+++ ptcg-server/src/sets/11-mega-evolution/set-pitch-black/other-prints.ts
@@ -0,0 +1,223 @@
+import { BraveBangle } from '../../10-scarlet-and-violet/set-black-bolt-white-flare/brave-bangle';
+import { CrushingHammer } from '../../10-scarlet-and-violet/set-scarlet-and-violet/crushing-hammer';
+import { EnergySwitch } from '../../10-scarlet-and-violet/set-scarlet-and-violet/energy-switch';
+import { IronDefender } from '../set-mega-evolution/iron-defender';
+import { Armarouge } from './armarouge';
+import { Bastiodon } from './bastiodon';
+import { DarkBell } from './dark-bell';
+import { Dhelmise } from './dhelmise';
+import { Fomantis } from './fomantis';
+import { GladionsFinalBattle } from './gladions-final-battle';
+import { Goldeen } from './goldeen';
+import { Gwynn } from './gwynn';
+import { Lurantisex } from './lurantis-ex';
+import { Manectric } from './manectric';
+import { MegaChandelureex } from './mega-chandelure-ex';
+import { MegaDarkraiex } from './mega-darkrai-ex';
+import { MegaExcadrillex } from './mega-excadrill-ex';
+import { MegaZeraoraex } from './mega-zeraora-ex';
+import { MistysVitality } from './mistys-vitality';
+import { Morpekoex } from './morpeko-ex';
+import { Primarina } from './primarina';
+import { Rampardosex } from './rampardos-ex';
+import { RustSyndicateGrunt } from './rust-syndicate-grunt';
+import { Silvally } from './silvally';
+import { Slowbro } from './slowbro';
+import { Thievul } from './thievul';
+import { Toucannon } from './toucannon';
+import { TremendousBomb } from './tremendous-bomb';
+import { Wailordex } from './wailord-ex';
+
+// MARK: Illustration rares
+
+export class FomantisIR extends Fomantis {
+  public fullName = 'Fomantis IR PBL';
+  public setNumber = '85';
+}
+
+export class ArmarougeIR extends Armarouge {
+  public fullName = 'Armarouge IR PBL';
+  public setNumber = '86';
+}
+
+export class GoldeenIR extends Goldeen {
+  public fullName = 'Goldeen IR PBL';
+  public setNumber = '87';
+}
+
+export class PrimarinaIR extends Primarina {
+  public fullName = 'Primarina IR PBL';
+  public setNumber = '88';
+}
+
+export class ManectricIR extends Manectric {
+  public fullName = 'Manectric IR PBL';
+  public setNumber = '89';
+}
+
+export class SlowbroIR extends Slowbro {
+  public fullName = 'Slowbro IR PBL';
+  public setNumber = '90';
+}
+
+export class DhelmiseIR extends Dhelmise {
+  public fullName = 'Dhelmise IR PBL';
+  public setNumber = '91';
+}
+
+export class ThievulIR extends Thievul {
+  public fullName = 'Thievul IR PBL';
+  public setNumber = '92';
+}
+
+export class BastiodonIR extends Bastiodon {
+  public fullName = 'Bastiodon IR PBL';
+  public setNumber = '93';
+}
+
+export class ToucannonIR extends Toucannon {
+  public fullName = 'Toucannon IR PBL';
+  public setNumber = '94';
+}
+
+export class SilvallyIR extends Silvally {
+  public fullName = 'Silvally IR PBL';
+  public setNumber = '95';
+}
+
+// MARK: Full art Pokémon ex
+
+export class LurantisexFA extends Lurantisex {
+  public fullName = 'Lurantis ex FA PBL';
+  public setNumber = '96';
+}
+
+export class WailordexFA extends Wailordex {
+  public fullName = 'Wailord ex FA PBL';
+  public setNumber = '97';
+}
+
+export class MegaZeraoraexFA extends MegaZeraoraex {
+  public fullName = 'Mega Zeraora ex FA PBL';
+  public setNumber = '98';
+}
+
+export class MegaChandelureexFA extends MegaChandelureex {
+  public fullName = 'Mega Chandelure ex FA PBL';
+  public setNumber = '99';
+}
+
+export class RampardosexFA extends Rampardosex {
+  public fullName = 'Rampardos ex FA PBL';
+  public setNumber = '100';
+}
+
+export class MegaDarkraiexFA extends MegaDarkraiex {
+  public fullName = 'Mega Darkrai ex FA PBL';
+  public setNumber = '101';
+}
+
+export class MorpekoexFA extends Morpekoex {
+  public fullName = 'Morpeko ex FA PBL';
+  public setNumber = '102';
+}
+
+export class MegaExcadrillexFA extends MegaExcadrillex {
+  public fullName = 'Mega Excadrill ex FA PBL';
+  public setNumber = '103';
+}
+
+// MARK: Full art Trainers
+
+export class BraveBangleFAPBL extends BraveBangle {
+  public fullName = 'Brave Bangle FA PBL';
+  public set = 'PBL';
+  public setNumber = '104';
+}
+
+export class CrushingHammerFAPBL extends CrushingHammer {
+  public fullName = 'Crushing Hammer FA PBL';
+  public set = 'PBL';
+  public setNumber = '105';
+}
+
+export class DarkBellFA extends DarkBell {
+  public fullName = 'Dark Bell FA PBL';
+  public setNumber = '106';
+}
+
+export class EnergySwitchFAPBL extends EnergySwitch {
+  public fullName = 'Energy Switch FA PBL';
+  public set = 'PBL';
+  public setNumber = '107';
+}
+
+export class GladionsFinalBattleFA extends GladionsFinalBattle {
+  public fullName = "Gladion's Final Battle FA PBL";
+  public setNumber = '108';
+}
+
+export class GwynnFA extends Gwynn {
+  public fullName = 'Gwynn FA PBL';
+  public setNumber = '109';
+}
+
+export class IronDefenderFAPBL extends IronDefender {
+  public fullName = 'Iron Defender FA PBL';
+  public set = 'PBL';
+  public setNumber = '110';
+}
+
+export class MistysVitalityFA extends MistysVitality {
+  public fullName = "Misty's Vitality FA PBL";
+  public setNumber = '111';
+}
+
+export class RustSyndicateGruntFA extends RustSyndicateGrunt {
+  public fullName = 'Rust Syndicate Grunt FA PBL';
+  public setNumber = '112';
+}
+
+export class TremendousBombFA extends TremendousBomb {
+  public fullName = 'Tremendous Bomb FA PBL';
+  public setNumber = '113';
+}
+
+// MARK: SIRs
+
+export class MegaZeraoraexSIR extends MegaZeraoraex {
+  public fullName = 'Mega Zeraora ex SIR PBL';
+  public setNumber = '114';
+}
+
+export class MegaChandelureexSIR extends MegaChandelureex {
+  public fullName = 'Mega Chandelure ex SIR PBL';
+  public setNumber = '115';
+}
+
+export class MegaDarkraiexSIR extends MegaDarkraiex {
+  public fullName = 'Mega Darkrai ex SIR PBL';
+  public setNumber = '116';
+}
+
+export class MorpekoexSIR extends Morpekoex {
+  public fullName = 'Morpeko ex SIR PBL';
+  public setNumber = '117';
+}
+
+export class GladionsFinalBattleSIR extends GladionsFinalBattle {
+  public fullName = "Gladion's Final Battle SIR PBL";
+  public setNumber = '118';
+}
+
+export class GwynnSIR extends Gwynn {
+  public fullName = 'Gwynn SIR PBL';
+  public setNumber = '119';
+}
+
+// MARK: Golds
+
+export class MegaDarkraiexHR extends MegaDarkraiex {
+  public fullName = 'Mega Darkrai ex HR PBL';
+  public setNumber = '120';
+}
