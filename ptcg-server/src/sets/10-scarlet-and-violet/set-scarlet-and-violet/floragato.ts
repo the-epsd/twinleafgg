4c1ec8f61fd79d55fea1d768932d00eee22561c5
Joe Myerscough <joey.myerscough@gmail.com>
2026-07-31 03:58:23 -0400

    add(cards): Sprigatito SVI, Floragato SVI

---
+++ ptcg-server/src/sets/10-scarlet-and-violet/set-scarlet-and-violet/floragato.ts
@@ -0,0 +1,31 @@
+import { PokemonCard } from '../../../game/store/card/pokemon-card';
+import { CardType, Stage } from '../../../game/store/card/card-types';
+
+export class Floragato extends PokemonCard {
+  public stage: Stage = Stage.STAGE_1;
+  public evolvesFrom: string = 'Sprigatito';
+  public hp: number = 90;
+  public cardType: CardType = G;
+  public weakness = [{ type: R }];
+  public retreat = [C];
+
+  public attacks = [{
+    name: 'Slash',
+    cost: [C],
+    damage: 20,
+    text: ''
+  },
+  {
+    name: 'Leaf Step',
+    cost: [G, C],
+    damage: 60,
+    text: ''
+  }];
+
+  public regulationMark = 'G';
+  public set: string = 'SVI';
+  public cardImage: string = 'assets/cardback.png';
+  public setNumber: string = '14';
+  public name: string = 'Floragato';
+  public fullName: string = 'Floragato SVI';
+}
