fefccd09113c8329c0489318c7c22b4e63b30c07
Eskaven <skelothan@gmail.com>
2026-08-29 14:52:50 -0700

    fix(cares): Petilil NVI set number

--- ptcg-server/src/sets/06-black-and-white/set-noble-victories/petilil.ts
+++ ptcg-server/src/sets/06-black-and-white/set-noble-victories/petilil.ts
@@ -1,59 +1,44 @@
 import { CardType, Stage } from '../../../game/store/card/card-types';
 import { PokemonCard } from '../../../game/store/card/pokemon-card';
-import { HealTargetEffect } from '../../../game/store/effects/attack-effects';
 import { Effect } from '../../../game/store/effects/effect';
+import { HEAL_X_DAMAGE_FROM_THIS_POKEMON } from '../../../game/store/prefabs/prefabs';
 import { WAS_ATTACK_USED } from '../../../game/store/prefabs/prefabs';
 import { State } from '../../../game/store/state/state';
 import { StoreLike } from '../../../game/store/store-like';
 
-
 export class Petilil extends PokemonCard {
-
   public stage: Stage = Stage.BASIC;
-
-  public cardType: CardType = CardType.GRASS;
-
+  public cardType: CardType = G;
   public hp: number = 60;
+  public weakness = [{ type: R }];
+  public resistance = [{ type: W, value: -20 }];
+  public retreat = [C];
 
-  public weakness = [{ type: CardType.FIRE }];
-
-  public resistance = [{
-    type: CardType.WATER,
-    value: -20
-  }];
-
-  public retreat = [CardType.COLORLESS];
-
-  public attacks = [{
-    name: 'Ram',
-    cost: [CardType.GRASS],
-    damage: 10,
-    text: ''
-  }, {
-    name: 'Absorb',
-    cost: [CardType.GRASS, CardType.COLORLESS],
-    damage: 10,
-    text: 'Heal 10 damage from this Pokémon.'
-  }];
+  public attacks = [
+    {
+      name: 'Ram',
+      cost: [C],
+      damage: 10,
+      text: '',
+    },
+    {
+      name: 'Absorb',
+      cost: [G, C],
+      damage: 10,
+      text: 'Heal 10 damage from this Pokémon.',
+    },
+  ];
 
   public set: string = 'NVI';
-
-  public setNumber: string = '23';
-
+  public setNumber: string = '4';
   public name: string = 'Petilil';
-
   public fullName: string = 'Petilil NVI';
 
   public reduceEffect(store: StoreLike, state: State, effect: Effect): State {
-
     if (WAS_ATTACK_USED(effect, 1, this)) {
-      const player = effect.player;
-      const healEffect = new HealTargetEffect(effect, 10);
-      healEffect.target = player.active;
-      return store.reduceEffect(state, healEffect);
+      HEAL_X_DAMAGE_FROM_THIS_POKEMON(effect, store, state, 10);
     }
 
     return state;
   }
-
 }
