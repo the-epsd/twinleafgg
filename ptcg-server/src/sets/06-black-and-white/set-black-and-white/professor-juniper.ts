c0de78a655429fe5ef8cfeb838b4580af7994163
Eskaven <skelothan@gmail.com>
2026-09-12 15:22:13 -0700

    fix(cards): Prof. Juniper canPlay

--- ptcg-server/src/sets/06-black-and-white/set-black-and-white/professor-juniper.ts
+++ ptcg-server/src/sets/06-black-and-white/set-black-and-white/professor-juniper.ts
@@ -7,23 +7,20 @@ import { StoreLike } from '../../../game/store/store-like';
 import { TrainerCard } from '../../../game/store/card/trainer-card';
 import { TrainerType } from '../../../game/store/card/card-types';
 import { DRAW_CARDS, MOVE_CARDS } from '../../../game/store/prefabs/prefabs';
+import { Player } from '../../../game';
 
 export class ProfessorJuniper extends TrainerCard {
-
   public trainerType: TrainerType = TrainerType.SUPPORTER;
-
   public set: string = 'BLW';
-
   public name: string = 'Professor Juniper';
-
   public fullName: string = 'Professor Juniper BLW';
-
   public cardImage: string = 'assets/cardback.png';
-
   public setNumber: string = '101';
+  public text: string = 'Discard your hand and draw 7 cards.';
 
-  public text: string =
-    'Discard your hand and draw 7 cards.';
+  public canPlay(_: StoreLike, __: State, player: Player): boolean {
+    return !(player.supporterTurn > 0 && player.deck.cards.length === 0);
+  }
 
   public reduceEffect(store: StoreLike, state: State, effect: Effect): State {
     if (effect instanceof TrainerEffect && effect.trainerCard === this) {
@@ -37,7 +34,7 @@ export class ProfessorJuniper extends TrainerCard {
         throw new GameError(GameMessage.CANNOT_PLAY_THIS_CARD);
       }
 
-      const cards = player.hand.cards.filter(c => c !== this);
+      const cards = player.hand.cards.filter((c) => c !== this);
       if (cards.length > 0) {
         state = MOVE_CARDS(store, state, player.hand, player.discard, { cards, sourceCard: this });
       }
@@ -46,5 +43,4 @@ export class ProfessorJuniper extends TrainerCard {
 
     return state;
   }
-
 }
